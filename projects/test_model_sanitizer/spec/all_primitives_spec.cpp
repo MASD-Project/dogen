542bd1a7608a0830c895291652faaa379685f5d1
Marco Craveiro <marco.craveiro@gmail.com>
2012-10-05 14:55:46 +0100

    tests: move test model code into a test model sanitizer spec
    - spec code can't really be mixed with generated code until we have
      support for ignoring certain folders; and even then it won't mix
      terribly well with the rebaselining infrastructure because the
      "actual" folder won't contain non-generated code. So we either
      codegen the spec or this won't work. A temporary hack was to create
      a spec dedicated to testing test models.
    - removed build_all target as its not used by CTest anymore now that
      we build packages. in addition, the run specs machinery with a clean
      of codegen was incorrect as the targets don't execute in
      order (obviously) so instead of misleading everyone, the madness has
      been (mostly) removed. We're back to a plain "run all specs"
      target. Once we start hitting codegen clean up problems again we
      need to look into fixing this properly.  I

--- projects/test_model_sanitizer/spec/all_primitives_spec.cpp
+++ projects/test_model_sanitizer/spec/all_primitives_spec.cpp
@@ -6,55 +6,53 @@
  * Licence. See doc/LICENCE.TXT for details.
  *
  */
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE dia_spec
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "dogen/utility/io/vector_io.hpp"
 #include "dogen/utility/test/logging.hpp"
 #include "dogen/utility/test/canned_tests.hpp"
-#include "dogen/class_in_a_package/domain/all.hpp"
-#include "dogen/class_in_a_package/io/all_io.hpp"
-#include "dogen/class_in_a_package/serialization/all_ser.hpp"
-#include "dogen/class_in_a_package/test_data/all_sequence.hpp"
-#include "dogen/class_in_a_package/hash/all_hash.hpp"
+#include "dogen/all_primitives/domain/all.hpp"
+#include "dogen/all_primitives/io/all_io.hpp"
+#include "dogen/all_primitives/serialization/all_ser.hpp"
+#include "dogen/all_primitives/test_data/all_sequence.hpp"
+#include "dogen/all_primitives/hash/all_hash.hpp"
 
 namespace {
 
 const std::string empty;
-const std::string test_module("dogen");
-const std::string test_suite("class_in_a_package");
+const std::string test_module("test_model_sanitizer");
+const std::string test_suite("all_primitives");
 
 }
 
-using namespace dogen::class_in_a_package;
+using namespace dogen::all_primitives;
 using namespace dogen::utility::test;
 
-BOOST_AUTO_TEST_SUITE(class_in_a_package)
+BOOST_AUTO_TEST_SUITE(all_primitives)
 
 BOOST_AUTO_TEST_CASE(validate_equality) {
     SETUP_TEST_LOG("validate_equality");
-    test_equality<package1::first_class_sequence>();
+    test_equality<a_class_sequence>();
     test_equality<versioned_key_sequence>();
     test_equality<unversioned_key_sequence>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_serialisation) {
     SETUP_TEST_LOG("validate_serialisation");
-    rountrip_type<package1::first_class_sequence>();
+    rountrip_type<a_class_sequence>();
     rountrip_type<versioned_key_sequence>();
     rountrip_type<unversioned_key_sequence>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_hashing) {
     SETUP_TEST_LOG("validate_hashing");
-    test_hashing<package1::first_class_sequence>();
+    test_hashing<a_class_sequence>();
     test_hashing<versioned_key_sequence>();
     test_hashing<unversioned_key_sequence>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_swap) {
     SETUP_TEST_LOG("validate_hashing");
-    test_swap<package1::first_class_sequence>();
+    test_swap<a_class_sequence>();
     test_swap<versioned_key_sequence>();
     test_swap<unversioned_key_sequence>();
 }
