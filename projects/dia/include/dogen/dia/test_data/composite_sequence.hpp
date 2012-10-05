3d20adda45768df210a9ee8d031b5d2db78a9dfc
Marco Craveiro <marco.craveiro@gmail.com>
2012-10-05 19:52:57 +0100

    licensing: update dia to GPLV v3

--- projects/dia/include/dogen/dia/test_data/composite_sequence.hpp
+++ projects/dia/include/dogen/dia/test_data/composite_sequence.hpp
@@ -1,9 +1,21 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * Copyright (C) 2012 Kitanda
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
  *
- * This file is distributed under the Kitanda Proprietary Software
- * Licence. See doc/LICENCE.TXT for details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Seethe
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General PublicLicense
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  *
  */
 #ifndef DOGEN_DIA_TEST_DATA_COMPOSITE_SEQUENCE_HPP
