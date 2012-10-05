874e0bc39730a7f8dbb407eef03c2c1e382b9cb9
Marco Craveiro <marco.craveiro@gmail.com>
2012-10-05 21:41:43 +0100

    licence: update generated cmake files to GPLV v3

--- projects/generator/src/backends/cpp/formatters/cpp_cmakelists.cpp
+++ projects/generator/src/backends/cpp/formatters/cpp_cmakelists.cpp
@@ -9,13 +9,13 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Seethe
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General PublicLicense
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  *
  */
 #include <sstream>
@@ -35,11 +35,30 @@ void cpp_cmakelists::format(view_models::cmakelists_view_model vm) {
     stream_ << "# -*- mode: cmake; tab-width: 4; indent-tabs-mode: nil -*-"
             << std::endl
             << "#" << std::endl
-            << "# Copyright (C) 2012 Kitanda" << std::endl
+            << "# Copyright (C) 2012 Kitanda <info@kitanda.co.uk>" << std::endl
             << "#" << std::endl
-            << "# This file is distributed under the Kitanda Proprietary Software"
+            << "# This program is free software; you can redistribute it and/or"
+            << " modify" << std::endl
+            << "# it under the terms of the GNU General Public License as"
+            << " published by" << std::endl
+            << "# the Free Software Foundation; either version 3 of the License"
+            << ", or" << std::endl
+            << "# (at your option) any later version." << std::endl
+            << "#" << std::endl
+            << "# This program is distributed in the hope that it will be "
+            << " useful," << std::endl
+            << "# but WITHOUT ANY WARRANTY; without even the implied warranty"
+            << " of" << std::endl
+            << "# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the"
             << std::endl
-            << "# Licence. See doc/LICENCE.TXT for details." << std::endl
+            << "# GNU General Public License for more details." << std::endl
+            << "#" << std::endl
+            << "# You should have received a copy of the GNU General "
+            << "Public License" << std::endl
+            << "# along with this program; if not, write to the Free Software"
+            << std::endl
+            << "# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,"
+            << std::endl << "# MA 02110-1301, USA." << std::endl
             << "#" << std::endl
             << "set(all_files \"\")" << std::endl
             << "file(GLOB_RECURSE all_files RELATIVE" << std::endl
