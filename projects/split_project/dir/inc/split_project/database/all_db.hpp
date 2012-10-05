11c23cfc6ca43381603c2ab490bceeef88b19761
Marco Craveiro <marco.craveiro@gmail.com>
2012-10-05 17:37:49 +0100

    generator: remove spurious files from split project test model

--- projects/split_project/dir/inc/split_project/database/all_db.hpp
+++
@@ -1,9 +0,0 @@
-/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * Copyright (C) 2012 Kitanda
- *
- * This file is distributed under the Kitanda Proprietary Software
- * Licence. See doc/LICENCE.TXT for details.
- *
- */
-#include "dogen/split_project/database/package/a_class_db.hpp"
