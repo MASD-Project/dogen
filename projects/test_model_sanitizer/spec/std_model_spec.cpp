cc58b03592364c5e43e69ae3024147f71a85d992
Marco Craveiro <marco.craveiro@gmail.com>
2012-11-23 13:15:55 +0000

    sanitizer: validate std::unordered_map support

--- projects/test_model_sanitizer/spec/std_model_spec.cpp
+++ projects/test_model_sanitizer/spec/std_model_spec.cpp
@@ -50,6 +50,7 @@ BOOST_AUTO_TEST_CASE(validate_equality) {
     test_equality<class_e_generator>();
     test_equality<class_f_generator>();
     test_equality<class_g_generator>();
+    test_equality<class_h_generator>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_serialisation) {
@@ -60,6 +61,7 @@ BOOST_AUTO_TEST_CASE(validate_serialisation) {
     rountrip_type<class_e_generator>();
     rountrip_type<class_f_generator>();
     rountrip_type<class_g_generator>();
+    rountrip_type<class_h_generator>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_hashing) {
@@ -70,6 +72,7 @@ BOOST_AUTO_TEST_CASE(validate_hashing) {
     test_hashing<class_e_generator>();
     test_hashing<class_f_generator>();
     test_hashing<class_g_generator>();
+    test_hashing<class_h_generator>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_swap) {
@@ -80,6 +83,7 @@ BOOST_AUTO_TEST_CASE(validate_swap) {
     test_swap<class_e_generator>();
     test_swap<class_f_generator>();
     test_swap<class_g_generator>();
+    test_swap<class_h_generator>();
 }
 
 BOOST_AUTO_TEST_CASE(validate_io) {
@@ -90,6 +94,7 @@ BOOST_AUTO_TEST_CASE(validate_io) {
     test_io<class_e_generator>();
     test_io<class_f_generator>();
     test_io<class_g_generator>();
+    test_io<class_h_generator>();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
