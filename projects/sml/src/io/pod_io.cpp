9c8a0ea00b673acfadf05797c5df4027c1286be7
Marco Craveiro <marco.craveiro@gmail.com>
2012-11-24 14:49:09 +0000

    sml: regenerate model with dogen 635

--- projects/sml/src/io/pod_io.cpp
+++ projects/sml/src/io/pod_io.cpp
@@ -74,6 +74,7 @@ inline std::ostream& operator<<(std::ostream& s, const std::list<dogen::sml::qua
 inline std::string tidy_up_string(std::string s) {
     boost::replace_all(s, "\r\n", "<new_line>");
     boost::replace_all(s, "\n", "<new_line>");
+    boost::replace_all(s, "\"", "<quote>");
     return s;
 }
 
