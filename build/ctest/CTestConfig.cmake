212ff634441f539036dbf29c542203d0e78903e2
Marco Craveiro <marco.craveiro@gmail.com>
2012-11-27 13:02:56 +0000

    cmake: deploy doxygen to new location

--- build/ctest/CTestConfig.cmake
+++ build/ctest/CTestConfig.cmake
@@ -28,6 +28,6 @@ set(CTEST_PROJECT_NAME "dogen")
 
 # kitware dashboard settings
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "http://hedgr.co.uk/cdash")
+set(CTEST_DROP_SITE "hedgr.co.uk/cdash")
 set(CTEST_DROP_LOCATION "/submit.php?project=dogen")
 set(CTEST_DROP_SITE_CDASH TRUE)
