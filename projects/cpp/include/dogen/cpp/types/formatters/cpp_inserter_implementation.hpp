29d5a57a9000ae6d82a0853de8dc61de99dbb6e0
Marco Craveiro <marco.craveiro@gmail.com>
2013-03-23 18:25:45 +0000

    cpp: copy across formatter headers

---
+++ projects/cpp/include/dogen/cpp/types/formatters/cpp_inserter_implementation.hpp
@@ -0,0 +1,83 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_CPP_TYPES_FORMATTERS_CPP_INSERTER_IMPLEMENTATION_HPP
+#define DOGEN_CPP_TYPES_FORMATTERS_CPP_INSERTER_IMPLEMENTATION_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <iosfwd>
+#include <string>
+#include <unordered_set>
+#include "dogen/cpp/types/view_models/nested_type_view_model.hpp"
+#include "dogen/cpp/types/view_models/class_view_model.hpp"
+#include "dogen/cpp/types/formatters/cpp_indenter.hpp"
+#include "dogen/cpp/types/formatters/cpp_utility.hpp"
+
+namespace dogen {
+namespace cpp {
+namespace formatters {
+
+class cpp_inserter_implementation {
+public:
+    typedef view_models::class_view_model class_view_model;
+    typedef view_models::nested_type_view_model nested_type_view_model;
+
+public:
+    cpp_inserter_implementation() = delete;
+    cpp_inserter_implementation(const cpp_inserter_implementation&) = default;
+    cpp_inserter_implementation(cpp_inserter_implementation&&) = default;
+    cpp_inserter_implementation& operator=(const cpp_inserter_implementation&) = default;
+
+public:
+    cpp_inserter_implementation(std::ostream& stream, cpp_indenter& indenter,
+        const bool is_inside_class);
+    virtual ~cpp_inserter_implementation() noexcept {}
+
+private:
+    bool is_insertable(const nested_type_view_model& vm);
+
+private:
+    void tidy_up_string_method();
+    void sequence_container_helper(const nested_type_view_model& vm);
+    void associative_container_helper(const nested_type_view_model& vm);
+    void smart_pointer_helper(const nested_type_view_model& vm);
+    void optional_helper(const nested_type_view_model& vm);
+    void pair_helper(const nested_type_view_model& vm);
+    void variant_helper(const nested_type_view_model& vm);
+    void recursive_helper_method_creator(const nested_type_view_model& vm,
+        std::unordered_set<std::string>& types_done);
+
+public:
+    void format_helper_methods(const class_view_model& vm);
+    void format_inserter_implementation(const class_view_model& vm);
+
+protected:
+    const bool is_inside_class_;
+    std::ostream& stream_;
+    cpp_indenter& indenter_;
+    cpp_utility utility_;
+};
+
+} } }
+
+#endif
---
+++ projects/cpp/include/dogen/cpp/types/formatters/cpp_serialization_implementation.hpp
@@ -0,0 +1,81 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_CPP_TYPES_FORMATTERS_CPP_SERIALIZATION_IMPLEMENTATION_HPP
+#define DOGEN_CPP_TYPES_FORMATTERS_CPP_SERIALIZATION_IMPLEMENTATION_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <iosfwd>
+#include <boost/filesystem/path.hpp>
+#include "dogen/cpp/types/view_models/file_view_model.hpp"
+#include "dogen/cpp/types/formatters/cpp_indenter.hpp"
+#include "dogen/cpp/types/formatters/cpp_utility.hpp"
+#include "dogen/cpp/types/formatters/file_formatter.hpp"
+
+namespace dogen {
+namespace cpp {
+namespace formatters {
+
+class serialization_implementation : public file_formatter {
+public:
+    typedef view_models::class_view_model class_view_model;
+    typedef view_models::file_view_model file_view_model;
+
+public:
+    serialization_implementation() = delete;
+    serialization_implementation(const serialization_implementation&) = default;
+    serialization_implementation(serialization_implementation&&) = default;
+    serialization_implementation&
+    operator=(const serialization_implementation&) = default;
+
+public:
+    serialization_implementation(std::ostream& stream,
+        const bool disable_xml_serialization);
+    virtual ~serialization_implementation() noexcept {}
+
+public:
+    static file_formatter::shared_ptr create(std::ostream& stream,
+        const bool disable_xml_serialization);
+
+private:
+    void save_function(const class_view_model& vm);
+    void load_function(const class_view_model& vm);
+    void template_instantiations(const class_view_model& vm);
+
+private:
+    void format_class(const file_view_model& vm);
+    void format_enumeration(const file_view_model& vm);
+
+public:
+    virtual void format(const file_view_model& vm) override;
+
+private:
+    std::ostream& stream_;
+    cpp_indenter indenter_;
+    cpp_utility utility_;
+    const bool disable_xml_serialization_;
+};
+
+} } }
+
+#endif
