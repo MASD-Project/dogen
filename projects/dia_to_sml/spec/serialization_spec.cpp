5164b2a27377136b6b8ea05fc6f86ef210a8b69d
Marco Craveiro <marco.craveiro@gmail.com>
2013-04-28 00:54:58 +0100

    dia to sml: add canned tests

---
+++ projects/dia_to_sml/spec/serialization_spec.cpp
@@ -0,0 +1,58 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include <boost/test/unit_test.hpp>
+#include "dogen/utility/test/logging.hpp"
+#include "dogen/utility/test/canned_tests.hpp"
+#include "dogen/dia_to_sml/types/all.hpp"
+#include "dogen/dia_to_sml/io/all_io.hpp"
+#include "dogen/dia_to_sml/test_data/all_td.hpp"
+#include "dogen/dia_to_sml/serialization/all_ser.hpp"
+#include "dogen/dia_to_sml/serialization/registrar_ser.hpp"
+
+template<typename Archive> void register_types(Archive& ar) {
+    dogen::dia_to_sml::register_types<Archive>(ar);
+}
+
+namespace {
+
+const std::string empty;
+const std::string test_module("dia_to_sml");
+const std::string test_suite("serialization_spec");
+
+}
+
+using namespace dogen::dia_to_sml;
+using namespace dogen::utility::test;
+
+BOOST_AUTO_TEST_SUITE(serialization)
+
+BOOST_AUTO_TEST_CASE(validate_serialization) {
+    SETUP_TEST_LOG("validate_serialization");
+
+    rountrip_type<context_generator>();
+    rountrip_type<node_generator>();
+    rountrip_type<object_profile_generator>();
+    rountrip_type<object_types_generator>();
+    rountrip_type<processed_object_generator>();
+    rountrip_type<processed_property_generator>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ projects/dia_to_sml/spec/swap_spec.cpp
@@ -0,0 +1,52 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include <boost/test/unit_test.hpp>
+#include "dogen/utility/test/logging.hpp"
+#include "dogen/utility/test/canned_tests.hpp"
+#include "dogen/dia_to_sml/types/all.hpp"
+#include "dogen/dia_to_sml/io/all_io.hpp"
+#include "dogen/dia_to_sml/test_data/all_td.hpp"
+
+namespace {
+
+const std::string empty;
+const std::string test_module("dia_to_sml");
+const std::string test_suite("swap_spec");
+
+}
+
+using namespace dogen::dia_to_sml;
+using namespace dogen::utility::test;
+
+BOOST_AUTO_TEST_SUITE(swap)
+
+BOOST_AUTO_TEST_CASE(validate_swap) {
+    SETUP_TEST_LOG("validate_swap");
+
+    test_swap<context_generator>();
+    test_swap<node_generator>();
+    test_swap<object_profile_generator>();
+    test_swap<object_types_generator>();
+    test_swap<processed_object_generator>();
+    test_swap<processed_property_generator>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
