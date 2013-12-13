cb20c6c6a5b641faf1b513855803d3cb5eab7b50
Marco Craveiro <marco.craveiro@gmail.com>
2013-12-13 08:42:49 +0000

    sml: document the property indexer

--- projects/sml/include/dogen/sml/types/property_indexer.hpp
+++ projects/sml/include/dogen/sml/types/property_indexer.hpp
@@ -38,8 +38,67 @@ namespace sml {
  * @brief Information indexer that specialises in indexing
  * properties across the model.
  *
+ * @section sml_property_indexer_0 Model requirements
+ *
  * The concept indexer expects to receive a partial model such as ones
- * coming straight out of Dia to SML transformation.
+ * coming straight out of Dia to SML transformation. The indexing of
+ * concepts is expected to have taken place.
+ *
+ * @section sml_property_indexer_1 Types of properties
+ *
+ * There are three sets of properties the indexer is interested in:
+ *
+ * @li local properties;
+ * @li inherited properties;
+ * @li all properties.
+ *
+ * Lets cover each of these in more details.
+ *
+ * @subsection sml_property_indexer_11 Local properties
+ *
+ * On a natural, pre-indexed state, local properties encompass only
+ * the properties defined on the class directly. The indexer behaves
+ * differently depending on whether it is indexing an object or a
+ * concept.
+ *
+ * For objects, the job of the indexer is to expand the local
+ * properties to include all properties obtained by modeling
+ * concepts. This is because we treat them no differently from
+ * properties defined in the class for purposes of code generation. As
+ * we rely on concept expansion, all we need is the set of all
+ * properties of all concepts we model. Local properties are useful to
+ * define all member variables owned by a class.
+ *
+ * For concepts, local properties are untouched. This is for two
+ * reasons. First, because we rely on the local properties of concepts
+ * to process the local properties of objects, as explained above, via
+ * concept expansion; thus local properties provide a quick way to
+ * access all the properties of all concepts one models. The second
+ * reason is due to the representation of concepts in source code:
+ * there is no requirement for a "meta-concept" - something from which
+ * concepts get properties from. There is nothing above concepts that
+ * replicate the relationship we have between concepts and
+ * objects. When concepts get expressed in source code, all we require
+ * is the set of all properties for that concept (see below).
+ *
+ * @subsection sml_property_indexer_12 Inherited properties
+ *
+ * These provide an easy way to look-up which properties one has
+ * inherited and from whom. For objects, the inherited properties are
+ * the "all property" set of each parent (see below). They are useful
+ * to call parent constructors and the like.
+ *
+ * For concepts, inherited properties are not particularly useful. We
+ * still index them by, arbitrarily, adding all the local properties
+ * of the parent. However, we still haven't found a good use for them.
+ *
+ * @subsection sml_property_indexer_13 All properties
+ *
+ * The "all property" set contains every single property. It is the
+ * sum all the local properties with all of the inherited
+ * properties. For objects it is useful for full constructors. For
+ * concepts it is effectively the full interface of the concept.
+ *
  */
 class property_indexer {
 public:
