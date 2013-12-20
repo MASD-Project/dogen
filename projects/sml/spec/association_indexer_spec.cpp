775db6027efd38d8b1a63f367201164eb16c35ca
Marco Craveiro <marco.craveiro@gmail.com>
2013-12-20 19:20:26 +0000

    sml: finish copying across applicable extractor tests to association indexer

--- projects/sml/spec/association_indexer_spec.cpp
+++ projects/sml/spec/association_indexer_spec.cpp
@@ -446,8 +446,6 @@ BOOST_AUTO_TEST_CASE(object_with_object_property_results_in_expected_indices) {
     dogen::sml::association_indexer ind;
     ind.index(m);
     BOOST_LOG_SEV(lg, debug) << "after indexing: " << m;
-
-    BOOST_REQUIRE(m.objects().size() == 2);
     for (const auto& pair : m.objects()) {
         const auto& qn(pair.first);
         const auto& o(pair.second);
@@ -487,139 +485,173 @@ BOOST_AUTO_TEST_CASE(model_with_object_with_missing_object_property_throws) {
     BOOST_CHECK_EXCEPTION(i.index(m), indexing_error, c);
 }
 
-// BOOST_AUTO_TEST_CASE_IGNORE(dependency_graph_of_object_with_pair_property_has_expected_names_in_relationships) {
-//     SETUP_TEST_LOG_SOURCE("dependency_graph_of_object_with_pair_property_has_expected_names_in_relationships");
+BOOST_AUTO_TEST_CASE(object_with_std_pair_property_results_in_expected_indices) {
+    SETUP_TEST_LOG_SOURCE("object_with_std_pair_property_results_in_expected_indices");
 
-//     const auto ot(object_types::value_object);
-//     const auto pt(property_types::std_pair);
-//     const auto m(factory.object_with_property(ot, pt));
-//     BOOST_LOG_SEV(lg, debug) << "input model: " << m;
-//     BOOST_REQUIRE(m.objects().size() == 2);
+    const auto ot(object_types::value_object);
+    const auto pt(property_types::std_pair);
+    auto m(factory.object_with_property(ot, pt));
+    BOOST_LOG_SEV(lg, debug) << "input model: " << m;
 
-//     bool found(false);
-//     BOOST_REQUIRE(m.objects().size() == 2);
-//     for (const auto& pair : m.objects()) {
-//         const auto& qn(pair.first);
-//         const auto& o(pair.second);
+    dogen::sml::association_indexer ind;
+    ind.index(m);
+    BOOST_LOG_SEV(lg, debug) << "after indexing: " << m;
 
-//         auto i(o.relationships().find(ra));
-//         if (factory.is_type_name_n(0, qn)) {
-//             found = true;
-//             BOOST_REQUIRE(i != o.relationships().end());
-//             BOOST_CHECK(i->second.size() == 2);
+    bool found(false);
+    BOOST_REQUIRE(m.objects().size() == 2);
+    for (const auto& pair : m.objects()) {
+        const auto& qn(pair.first);
+        const auto& o(pair.second);
 
-//             i = o.relationships().find(pa);
-//             BOOST_REQUIRE(i != o.relationships().end());
-//             BOOST_CHECK(i->second.size() == 1);
-//         }
-//         ignore std pair
-//     }
-//     BOOST_CHECK(found);
-// }
+        using dogen::sml::relationship_types;
+        const auto ra(relationship_types::regular_associations);
+        const auto pa(relationship_types::pointer_associations);
 
-// BOOST_AUTO_TEST_CASE_IGNORE(dependency_graph_of_object_with_variant_property_has_expected_names_in_relationships) {
-//     SETUP_TEST_LOG_SOURCE("dependency_graph_of_object_with_variant_property_has_expected_names_in_relationships");
+        auto i(o.relationships().find(ra));
+        if (factory.is_type_name_n(0, qn)) {
+            found = true;
+            BOOST_REQUIRE(i != o.relationships().end());
+            BOOST_CHECK(i->second.size() == 2);
+            const auto front_sn(i->second.front().simple_name());
+            BOOST_CHECK(front_sn == "bool" || front_sn == "pair");
+            const auto back_sn(i->second.back().simple_name());
+            BOOST_CHECK(back_sn == "bool" || back_sn == "pair");
 
-//     const auto ot(object_types::value_object);
-//     const auto pt(property_types::boost_variant);
-//     const auto m(factory.object_with_property(ot, pt));
-//     BOOST_LOG_SEV(lg, debug) << "input model: " << m;
-//     BOOST_REQUIRE(m.objects().size() == 2);
-//     BOOST_REQUIRE(m.primitives().size() == 2);
+            i = o.relationships().find(pa);
+            BOOST_CHECK(i == o.relationships().end());
+        }
+        // ignore std pair, etc.
+    }
+    BOOST_CHECK(found);
+}
 
-//     bool found(false);
-//     dogen::sml_to_cpp::extractor x(m);
-//     for (const auto& pair : m.objects()) {
-//         if (factory.is_type_name_n(0, pair.first)) {
-//             BOOST_LOG_SEV(lg, debug) << "found object: " << pair.first;
-//             BOOST_REQUIRE(pair.second.local_properties().size() == 1);
-//             const auto r(x.extract_dependency_graph(pair.second));
-//             BOOST_LOG_SEV(lg, debug) << "relationships: " << r;
+BOOST_AUTO_TEST_CASE(object_with_boost_variant_property_results_in_expected_indices) {
+    SETUP_TEST_LOG_SOURCE("object_with_boost_variant_property_results_in_expected_indices");
 
-//             BOOST_REQUIRE(r.names().size() == 3);
-//             BOOST_CHECK(r.forward_decls().empty());
-//             BOOST_CHECK(r.keys().empty());
-//             BOOST_CHECK(r.leaves().empty());
-//             BOOST_CHECK(!r.has_std_string());
-//             BOOST_CHECK(r.has_variant());
-//             BOOST_CHECK(!r.is_parent());
-//             BOOST_CHECK(!r.is_child());
-//             BOOST_CHECK(r.requires_stream_manipulators());
-//             BOOST_CHECK(!r.has_std_pair());
-//             found = true;
-//         }
-//     }
-//     BOOST_CHECK(found);
-// }
+    const auto ot(object_types::value_object);
+    const auto pt(property_types::boost_variant);
+    auto m(factory.object_with_property(ot, pt));
+    BOOST_LOG_SEV(lg, debug) << "input model: " << m;
+    BOOST_REQUIRE(m.objects().size() == 2);
+    BOOST_REQUIRE(m.primitives().size() == 2);
 
-// BOOST_AUTO_TEST_CASE_IGNORE(dependency_graph_of_object_with_std_string_property_has_expected_name_in_relationships) {
-//     SETUP_TEST_LOG_SOURCE("dependency_graph_of_object_with_std_string_property_has_expected_name_in_relationships");
+    bool found(false);
+    dogen::sml::association_indexer ind;
+    ind.index(m);
+    BOOST_LOG_SEV(lg, debug) << "after indexing: " << m;
 
-//     const auto ot(object_types::value_object);
-//     const auto pt(property_types::std_string);
-//     const auto m(factory.object_with_property(ot, pt));
-//     BOOST_LOG_SEV(lg, debug) << "input model: " << m;
-//     BOOST_REQUIRE(m.objects().size() == 2);
+    for (const auto& pair : m.objects()) {
+        const auto& qn(pair.first);
+        const auto& o(pair.second);
 
-//     bool found(false);
-//     dogen::sml_to_cpp::extractor x(m);
-//     for (const auto& pair : m.objects()) {
-//         if (factory.is_type_name_n(0, pair.first)) {
-//             BOOST_LOG_SEV(lg, debug) << "found object: " << pair.first;
-//             BOOST_REQUIRE(pair.second.local_properties().size() == 1);
-//             const auto r(x.extract_dependency_graph(pair.second));
-//             BOOST_LOG_SEV(lg, debug) << "relationships: " << r;
+        if (factory.is_type_name_n(0, pair.first)) {
+            found = true;
+            BOOST_LOG_SEV(lg, debug) << "found object: " << qn;
 
-//             BOOST_REQUIRE(r.names().size() == 1);
-//             BOOST_CHECK(r.names().begin()->simple_name() == "string");
-//             BOOST_CHECK(r.forward_decls().empty());
-//             BOOST_CHECK(r.keys().empty());
-//             BOOST_CHECK(r.leaves().empty());
-//             BOOST_CHECK(r.has_std_string());
-//             BOOST_CHECK(!r.has_variant());
-//             BOOST_CHECK(!r.is_parent());
-//             BOOST_CHECK(!r.is_child());
-//             BOOST_CHECK(!r.requires_stream_manipulators());
-//             BOOST_CHECK(!r.has_std_pair());
-//             found = true;
-//         }
-//     }
-//     BOOST_CHECK(found);
-// }
+            using dogen::sml::relationship_types;
+            const auto ra(relationship_types::regular_associations);
+            const auto pa(relationship_types::pointer_associations);
 
-// BOOST_AUTO_TEST_CASE_IGNORE(dependency_graph_of_object_with_boost_shared_ptr_property_has_expected_name_in_relationships) {
-//     SETUP_TEST_LOG_SOURCE("dependency_graph_of_object_with_boost_shared_ptr_property_has_expected_name_in_relationships");
+            auto i(o.relationships().find(ra));
+            BOOST_REQUIRE(i != o.relationships().end());
+            BOOST_CHECK(i->second.size() == 3);
+            bool found_uint(false), found_bool(false), found_variant(false);
+            for (const auto& qn : i->second) {
+                const auto sn(qn.simple_name());
+                if (sn == "unsigned int")
+                    found_uint = true;
+                else if (sn == "bool")
+                    found_bool = true;
+                else if (sn == "variant")
+                    found_variant = true;
+            }
+            BOOST_CHECK(found_uint);
+            BOOST_CHECK(found_bool);
+            BOOST_CHECK(found_variant);
 
-//     const auto ot(object_types::value_object);
-//     const auto pt(property_types::boost_shared_ptr);
-//     const auto m(factory.object_with_property(ot, pt));
-//     BOOST_LOG_SEV(lg, debug) << "input model: " << m;
-//     BOOST_REQUIRE(m.objects().size() == 3);
+            i = o.relationships().find(pa);
+            BOOST_CHECK(i == o.relationships().end());
+        }
+    }
+    BOOST_CHECK(found);
+}
 
-//     bool found(false);
-//     dogen::sml_to_cpp::extractor x(m);
-//     for (const auto& pair : m.objects()) {
-//         if (factory.is_type_name_n(0, pair.first)) {
-//             BOOST_LOG_SEV(lg, debug) << "found object: " << pair.first;
-//             BOOST_REQUIRE(pair.second.local_properties().size() == 1);
+BOOST_AUTO_TEST_CASE(object_with_std_string_property_results_in_expected_indices) {
+    SETUP_TEST_LOG_SOURCE("object_with_std_string_property_results_in_expected_indices");
 
-//             found = true;
-//             const auto r(x.extract_dependency_graph(pair.second));
-//             BOOST_LOG_SEV(lg, debug) << "relationships: " << r;
+    const auto ot(object_types::value_object);
+    const auto pt(property_types::std_string);
+    auto m(factory.object_with_property(ot, pt));
+    BOOST_LOG_SEV(lg, debug) << "input model: " << m;
 
-//             BOOST_REQUIRE(r.names().size() == 1);
-//             BOOST_CHECK(r.forward_decls().size() == 1);
-//             BOOST_CHECK(r.keys().empty());
-//             BOOST_CHECK(r.leaves().empty());
-//             BOOST_CHECK(!r.has_std_string());
-//             BOOST_CHECK(!r.has_variant());
-//             BOOST_CHECK(!r.is_parent());
-//             BOOST_CHECK(!r.is_child());
-//             BOOST_CHECK(!r.requires_stream_manipulators());
-//             BOOST_CHECK(!r.has_std_pair());
-//         }
-//     }
-//     BOOST_CHECK(found);
-// }
+    dogen::sml::association_indexer ind;
+    ind.index(m);
+    BOOST_LOG_SEV(lg, debug) << "after indexing: " << m;
+
+    bool found(false);
+    BOOST_REQUIRE(m.objects().size() == 2);
+    for (const auto& pair : m.objects()) {
+        const auto& qn(pair.first);
+        const auto& o(pair.second);
+
+        using dogen::sml::relationship_types;
+        const auto ra(relationship_types::regular_associations);
+        const auto pa(relationship_types::pointer_associations);
+
+        auto i(o.relationships().find(ra));
+        if (factory.is_type_name_n(0, qn)) {
+            found = true;
+            BOOST_REQUIRE(i != o.relationships().end());
+            BOOST_REQUIRE(i->second.size() == 1);
+            BOOST_CHECK(i->second.front().simple_name() == "string");
+
+            i = o.relationships().find(pa);
+            BOOST_CHECK(i == o.relationships().end());
+        }
+        // ignore std pair, etc.
+    }
+    BOOST_CHECK(found);
+}
+
+BOOST_AUTO_TEST_CASE(object_with_boost_shared_ptr_property_results_in_expected_indices) {
+    SETUP_TEST_LOG_SOURCE("object_with_boost_shared_ptr_property_results_in_expected_indices");
+
+    const auto ot(object_types::value_object);
+    const auto pt(property_types::boost_shared_ptr);
+    auto m(factory.object_with_property(ot, pt));
+    BOOST_LOG_SEV(lg, debug) << "input model: " << m;
+    BOOST_REQUIRE(m.objects().size() == 3);
+
+    bool found(false);
+    dogen::sml::association_indexer ind;
+    ind.index(m);
+    BOOST_LOG_SEV(lg, debug) << "after indexing: " << m;
+
+    for (const auto& pair : m.objects()) {
+        const auto& qn(pair.first);
+        const auto& o(pair.second);
+
+        if (factory.is_type_name_n(0, pair.first)) {
+            found = true;
+            BOOST_LOG_SEV(lg, debug) << "found object: " << qn;
+
+            using dogen::sml::relationship_types;
+            const auto ra(relationship_types::regular_associations);
+            const auto pa(relationship_types::pointer_associations);
+
+            auto i(o.relationships().find(ra));
+            BOOST_REQUIRE(i != o.relationships().end());
+            BOOST_REQUIRE(i->second.size() == 1);
+            BOOST_CHECK(i->second.front().simple_name() == "shared_ptr");
+
+            i = o.relationships().find(pa);
+            BOOST_CHECK(i != o.relationships().end());
+            BOOST_REQUIRE(i->second.size() == 1);
+            BOOST_CHECK(factory.is_type_name_n(1,
+                    i->second.front().simple_name()));
+        }
+    }
+    BOOST_CHECK(found);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
