8d2847737ffe6c475b862a939521a66674d5d53d
Marco Craveiro <marco.craveiro@gmail.com>
2014-02-03 23:43:45 +0000

    sanity: random ruby tests

---
+++ projects/sanity/bin/test.rb
@@ -0,0 +1,18 @@
+class BookInStock
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = Float(price)
+    end
+
+    def to_s
+        "ISBN: #{@isbn}, price: #{@price}"
+    end
+
+end
+
+b1 = BookInStock.new("isbn1", 3)
+p b1
+b2 = BookInStock.new("isbn2", 3.14)
+p b2
+b3 = BookInStock.new("isbn3", 5.67)
+p b3
