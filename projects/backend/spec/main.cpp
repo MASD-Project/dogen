1b64904e67024cce02dfa6071740d47767b42e2d
Marco Craveiro <marco.craveiro@gmail.com>
2014-10-06 19:27:19 +0100

    backend: fix compilation errors

--- projects/backend/spec/main.cpp
+++ projects/backend/spec/main.cpp
@@ -19,7 +19,7 @@
  *
  */
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE frontend_spec
+#define BOOST_TEST_MODULE backend_spec
 #include <iostream>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/unit_test_monitor.hpp>
