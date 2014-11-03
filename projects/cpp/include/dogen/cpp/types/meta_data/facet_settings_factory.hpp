05a22b59eb5d08bcd4f938a3ed6371330c81910f
Marco Craveiro <marco.craveiro@gmail.com>
2014-11-03 23:25:11 +0000

    cpp: add a skeleton for a facet settings factory

---
+++ projects/cpp/include/dogen/cpp/types/meta_data/facet_settings_factory.hpp
@@ -0,0 +1,53 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_CPP_TYPES_META_DATA_FACET_SETTINGS_FACTORY_HPP
+#define DOGEN_CPP_TYPES_META_DATA_FACET_SETTINGS_FACTORY_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include "dogen/cpp/types/facet_settings.hpp"
+
+namespace dogen {
+namespace cpp {
+namespace meta_data {
+
+class facet_settings_factory {
+public:
+    facet_settings_factory() = delete;
+    facet_settings_factory(const facet_settings_factory&) = default;
+    facet_settings_factory(facet_settings_factory&&) = default;
+    ~facet_settings_factory() = default;
+
+public:
+    /**
+     * @brief Builds the facet settings for facet from the meta data.
+     */
+    facet_settings build(const std::string& facet_id,
+        const boost::property_tree::ptree& meta_data) const;
+};
+
+} } }
+
+#endif
--- projects/cpp/src/types/meta_data/facet_settings_factory.cpp
+++ projects/cpp/src/types/meta_data/facet_settings_factory.cpp
@@ -1,2 +1,43 @@
-// dummy function to suppress ranlib warnings
-void facet_settings_factory() { }
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012 Kitanda <info@kitanda.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include "dogen/utility/log/logger.hpp"
+#include "dogen/sml/types/meta_data/reader.hpp"
+#include "dogen/cpp/types/meta_data/traits.hpp"
+#include "dogen/cpp/types/meta_data/facet_settings_factory.hpp"
+
+namespace {
+
+using namespace dogen::utility::log;
+auto lg(logger_factory("cpp.meta_data.facet_settings_factory"));
+
+}
+
+namespace dogen {
+namespace cpp {
+namespace meta_data {
+
+facet_settings facet_settings_factory::build(const std::string& /*facet_id*/,
+    const boost::property_tree::ptree& /*meta_data*/) const {
+    facet_settings r;
+    return r;
+}
+
+} } }
