3b50b79a049ac53970014ad7eb4dd344137056a7
Marco Craveiro <marco.craveiro@gmail.com>
2015-06-09 18:09:17 +0100

    cpp: work on inserter method helper template

--- projects/cpp/src/types/formatters/io/inserter_implementation_helper_stitch.cpp
+++ projects/cpp/src/types/formatters/io/inserter_implementation_helper_stitch.cpp
@@ -38,28 +38,29 @@ fa.stream() << "    s.precision(6);" << std::endl;
 fa.stream() << "    s.setf(std::ios::showpoint);" << std::endl;
     }
 
-    const bool no_parents_or_properties(c.parents().empty() &&
+
+    const bool no_parents_and_no_properties(c.parents().empty() &&
         c.all_properties().empty());
 fa.stream() << "    s << \" { \"" << std::endl;
-fa.stream() << "      << \"\\\"__type__\\\": \" << \"\\\"" << c.qualified_name() << "\\\"\"" << (no_parents_or_properties ? " << \" }\";" : "") << std::endl;
-    dogen::formatters::sequence_formatter sf(c.properties().size());
-    sf.element_separator(" << \", \"");
-    sf.prefix_configuration()
-        .first("  << \", \" << ")
-        .not_first("s << \", \" << ");
+fa.stream() << "      << \"\\\"__type__\\\": \" << \"\\\"" << c.qualified_name() << "\\\"\"" << (no_parents_and_no_properties ? " << \" }\";" : " << \", \"") << std::endl;
+
+    dogen::formatters::sequence_formatter sf(c.parents().size());
+    sf.prefix_configuration().first("  << ").not_first("s << ");
+    sf.element_separator("");
     for (const auto p : c.parents()) {
-fa.stream() << "    " << sf.prefix() << "\"\\\"__parent_\" << " << sf.current_position() << " << \"__\\\": \"" << sf.postfix(true/*skip*/) << ";" << std::endl;
+fa.stream() << "    " << sf.prefix() << "\"\\\"__parent_" << sf.current_position() << "__\\\": \"" << sf.postfix() << ";" << std::endl;
 fa.stream() << "    " << p.name() << "::to_stream(s);" << std::endl;
     }
 
-    sf.reset();
-    sf.prefix_configuration()
-        .first(c.parents().empty() ? "  << \", \" << " : "s << \", \" << ")
-        .not_first("  << \", \" << ");
-    sf.postfix_configuration().last("<< \" } \";");
     auto ntfa(fa.make_nested_type_formatting_assistant());
     for (const auto p : c.properties()) {
-fa.stream() << "    " << sf.prefix() << "\"\\\"" << p.name() << "\\\": \" << " << ntfa.streaming_for_type(p.type(), "v") << sf.postfix() << std::endl;
+fa.stream() << "    s << \", \"" << std::endl;
+fa.stream() << "      << \"\\\"" << p.name() << "\\\": \" << " << ntfa.streaming_for_type(p.type(), "v") << std::endl;
     }
+
+    if (!c.properties().empty())
+fa.stream() << "      << \" }\";" << std::endl;
+    else
+fa.stream() << "    s << \" }\";" << std::endl;
 }
 } } } }
