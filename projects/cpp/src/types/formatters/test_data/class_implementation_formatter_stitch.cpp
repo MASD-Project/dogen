cd234ac566e4908960f2c43d6e6903deb883b3f1
Marco Craveiro <marco.craveiro@gmail.com>
2015-06-18 08:44:00 +0100

    cpp: more work on test data's class implementation formatter

--- projects/cpp/src/types/formatters/test_data/class_implementation_formatter_stitch.cpp
+++ projects/cpp/src/types/formatters/test_data/class_implementation_formatter_stitch.cpp
@@ -58,7 +58,85 @@ fa.stream() << std::endl;
 fa.stream() << std::endl;
 fa.stream() << "}" << std::endl;
             }
+fa.stream() << c.name() << "_generator::" << c.name() << "_generator() : position_(0) { }" << std::endl;
 fa.stream() << std::endl;
+            /*
+             * Populate method.
+             */
+            if (!c.is_immutable()) {
+                bool no_args(c.properties().empty() && c.parents().empty());
+                if (no_args)
+fa.stream() << "void " << c.name() << "_generator populate(const unsigned int /*position*/, result_type& /*v*/) {" << std::endl;
+                else
+fa.stream() << "void " << c.name() << "_generator populate(const unsigned int position, result_type& v) {" << std::endl;
+                for (const auto p : c.parents()) {
+fa.stream() << "    " << p.qualified_name() << "_generator::populate(position, v);" << std::endl;
+                }
+                unsigned int i(0);
+                for (const auto p : c.properties()) {
+fa.stream() << "    v." << p.name() << "(create_" << p.type().complete_identifiable_name() << "(position + " << i << "));" << std::endl;
+                    ++i;
+                }
+            }
+fa.stream() << "}" << std::endl;
+            /*
+             * Create method.
+             */
+            if (c.is_parent()) {
+                 const bool no_arg(c.all_properties().empty());
+fa.stream() << c.name() << "_generator::result_type" << std::endl;
+fa.stream() << c.name() << "::create(const unsigned int" << (no_arg ? "/*position*/" : " position") << ") {" << std::endl;
+                if (c.is_immutable()) {
+fa.stream() << "    return " << c.name() << "(" << std::endl;
+                    unsigned int i(0);
+                    for (const auto p : c.properties()) {
+fa.stream() << "        create_" << p.type().complete_identifiable_name() << "(position + " << i << ")" << std::endl;
+                    }
+fa.stream() << "        );" << std::endl;
+                } else {
+fa.stream() << "    " << c.name() << " r;" << std::endl;
+                    if (!c.all_properties().empty())
+fa.stream() << "    " << c.name() << "::populate(position, r);" << std::endl;
+fa.stream() << "    return r;" << std::endl;
+                }
+fa.stream() << "}" << std::endl;
+            }
+
+            /*
+             * Create method ptr.
+             */
+fa.stream() << c.name() << "_generator::result_type*" << std::endl;
+fa.stream() << c.name() << "::create_ptr(const unsigned int position) {" << std::endl;
+            if (c.leaves().empty()) {
+                if (c.is_immutable())
+fa.stream() << "    return new " << c.name() << "(create(position));" << std::endl;
+                else {
+fa.stream() << "    " << c.name() << "* p = new " << c.name() << "();" << std::endl;
+fa.stream() << "    " << c.name() << "_generator::populate(position, *p);" << std::endl;
+fa.stream() << "    return p;  " << std::endl;
+                }
+            } else {
+                auto leaves(c.leaves());
+                const auto front(leaves.front());
+                leaves.pop_front();
+                unsigned int i(0);
+                unsigned int total(leaves.size());
+                for (const auto l : leaves) {
+fa.stream() << "    if ((position % " << total << ") == " << i++ << ")" << std::endl;
+fa.stream() << "        return " << l << "_generator::create_ptr(position);" << std::endl;
+                }
+fa.stream() << "    return " << front << "_generator::create_ptr(position);" << std::endl;
+            }
+
+            /*
+             * Function operator
+             */
+             if (!c.is_parent()) {
+fa.stream() << c.name() << "_generator::result_type" << std::endl;
+fa.stream() << "::operator()() {" << std::endl;
+fa.stream() << "    return create(position_++);" << std::endl;
+fa.stream() << "}" << std::endl;
+            }
         } // snf
     } // sbf
     // return fa.make_file();
