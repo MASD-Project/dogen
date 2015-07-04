cc8002ad28240d62db21b6326adac077eabca68b
Marco Craveiro <marco.craveiro@gmail.com>
2015-07-04 22:12:33 +0100

    cpp: implement register method for leaves

--- projects/cpp/src/types/formatters/serialization/registrar_implementation_formatter_stitch.cpp
+++ projects/cpp/src/types/formatters/serialization/registrar_implementation_formatter_stitch.cpp
@@ -29,35 +29,48 @@ namespace serialization {
 
 dogen::formatters::file registrar_implementation_formatter_stitch(
     formatters::entity_formatting_assistant& fa,
-    const formattables::registrar_info& /*ri*/) {
+    const formattables::registrar_info& ri) {
 
     {
         auto sbf(fa.make_scoped_boilerplate_formatter());
         {
             auto snf(fa.make_scoped_namespace_formatter());
+            const auto deps(ri.model_dependencies());
+            const auto leaves(ri.leaves());
+            const bool has_types(!deps.empty() || !leaves.empty());
+            const std::string arg_name(has_types ? " ar" : "");
 fa.stream() << std::endl;
 fa.stream() << "template<typename Archive>" << std::endl;
-fa.stream() << "void register_types(Archive&) {" << std::endl;
+fa.stream() << "void register_types(Archive&" << arg_name << ") {" << std::endl;
+            if (has_types) {
+                for (const auto d : deps)
+fa.stream() << "    " << d << "::register_types(ar);" << std::endl;
+                if (!deps.empty() && !leaves.empty())
+fa.stream() << std::endl;
+                for (const auto l : leaves)
+fa.stream() << "    ar.template register_type<" << l << ">();" << std::endl;
+
+            }
 fa.stream() << "}" << std::endl;
 fa.stream() << std::endl;
-fa.stream() << "template void register_types(boost::archive::polymorphic_oarchive&);" << std::endl;
-fa.stream() << "template void register_types(boost::archive::polymorphic_iarchive&);" << std::endl;
+fa.stream() << "template void register_types(boost::archive::polymorphic_oarchive&" << arg_name << ");" << std::endl;
+fa.stream() << "template void register_types(boost::archive::polymorphic_iarchive&" << arg_name << ");" << std::endl;
 fa.stream() << std::endl;
-fa.stream() << "template void register_types(boost::archive::text_oarchive&);" << std::endl;
-fa.stream() << "template void register_types(boost::archive::text_iarchive&);" << std::endl;
+fa.stream() << "template void register_types(boost::archive::text_oarchive&" << arg_name << ");" << std::endl;
+fa.stream() << "template void register_types(boost::archive::text_iarchive&" << arg_name << ");" << std::endl;
 fa.stream() << std::endl;
-fa.stream() << "template void register_types(boost::archive::binary_oarchive&);" << std::endl;
-fa.stream() << "template void register_types(boost::archive::binary_iarchive&);" << std::endl;
+fa.stream() << "template void register_types(boost::archive::binary_oarchive&" << arg_name << ");" << std::endl;
+fa.stream() << "template void register_types(boost::archive::binary_iarchive&" << arg_name << ");" << std::endl;
 fa.stream() << std::endl;
             if (!fa.is_xml_serialization_disabled()) {
-fa.stream() << "template void register_types(boost::archive::xml_oarchive&);" << std::endl;
-fa.stream() << "template void register_types(boost::archive::xml_iarchive&);" << std::endl;
+fa.stream() << "template void register_types(boost::archive::xml_oarchive&" << arg_name << ");" << std::endl;
+fa.stream() << "template void register_types(boost::archive::xml_iarchive&" << arg_name << ");" << std::endl;
 fa.stream() << std::endl;
             }
 
             if (!fa.is_eos_serialization_disabled()) {
-fa.stream() << "template void register_types(eos::portable_oarchive&);" << std::endl;
-fa.stream() << "template void register_types(eos::portable_iarchive&);" << std::endl;
+fa.stream() << "template void register_types(eos::portable_oarchive&" << arg_name << ");" << std::endl;
+fa.stream() << "template void register_types(eos::portable_iarchive&" << arg_name << ");" << std::endl;
 fa.stream() << std::endl;
             }
         } // snf
