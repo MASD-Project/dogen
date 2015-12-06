c227c4f32ef049213d18a46f2955b145ccb9dbad
Marco Craveiro <marco.craveiro@gmail.com>
2015-12-06 23:43:44 +0000

    yarn: improve model documentation

--- projects/yarn/include/dogen/yarn/types/yarn.hpp
+++ projects/yarn/include/dogen/yarn/types/yarn.hpp
@@ -28,23 +28,35 @@
 namespace dogen {
 
 /**
- * @brief Yarn is the dogen meta-model.
+ * @brief Yarn is the Dogen meta-model.
+ *
+ * @section yarn_0 Overview
+ *
+ * Yarn is a meta-model that provides a programming-language-neutral type
+ * representation, designed for code generation. There are two core types
+ * in yarn: @e intermediate_model and @e model. Intermediate models are
+ * obtained via @frontends and post-processed until they form a cohesive
+ * whole. They are then transformed into a final model representation, at
+ * which point they are suitable for code generation.
  *
  * @section yarn_0 Mission
  *
- * Yarn is a meta-model that provides a language neutral type representation.
- * Its objectives are:
+ * Yarn's objectives are:
  *
- * - to provide a set of constructs that the frontends can map to easily,
- *   as well as the machinery for frontend management (registration,
- *   etc.).
- * - to provide the tooling required to combine multiple intermediate
- *   yarn models together into a bigger model - called the intermediate @e merged
- *   model.
- *   - to ensure the consistency of the intermediate merged model via validation
- *   rules (such as verifying that all types have a definitions, etc.).
- * - to provide all the information required to build the final
- *   model and to build it.
+ * @li to provide a set of constructs that the frontends can map to
+ * easily, as well as the machinery for frontend management
+ * (registration, etc.).
+ *
+ * @li to provide the tooling required to combine multiple intermediate
+ * yarn models together into a bigger model - called the intermediate @e
+ * merged model.
+ *
+ * @li to ensure the consistency of the intermediate merged model via
+ * validation rules (such as verifying that all types have a definitions,
+ * etc.).
+ *
+ * @li to provide all the information required to build the final model
+ * and to build it.
  *
  */
 namespace yarn {
