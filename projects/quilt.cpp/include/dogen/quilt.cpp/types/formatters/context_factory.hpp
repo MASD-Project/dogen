5823c8cddb50ce7565b448ec54e8a221cef3bbc3
Marco Craveiro <marco.craveiro@gmail.com>
2016-02-11 23:22:30 +0000

    quilt cpp: create a context factory

---
+++ projects/quilt.cpp/include/dogen/quilt.cpp/types/formatters/context_factory.hpp
@@ -0,0 +1,73 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_QUILT_CPP_TYPES_FORMATTERS_CONTEXT_FACTORY_HPP
+#define DOGEN_QUILT_CPP_TYPES_FORMATTERS_CONTEXT_FACTORY_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include "dogen/yarn/types/name.hpp"
+#include "dogen/quilt.cpp/types/workflow_error.hpp"
+#include "dogen/quilt.cpp/types/settings/bundle_repository.hpp"
+#include "dogen/quilt.cpp/types/settings/helper_settings_repository.hpp"
+#include "dogen/quilt.cpp/types/formattables/formatter_properties_repository.hpp"
+#include "dogen/quilt.cpp/types/formatters/context.hpp"
+
+namespace dogen {
+namespace quilt {
+namespace cpp {
+namespace formatters {
+
+class context_factory {
+public:
+    context_factory(const settings::bundle_repository& brp,
+        const settings::helper_settings_repository& hsrp,
+        const formattables::formatter_properties_repository& fprp,
+        const std::unordered_map<
+        std::string, std::unordered_map<
+        std::string,
+        std::shared_ptr<formatter_helper_interface>>>& helpers);
+
+private:
+    const std::unordered_map<std::string,
+                             formattables::formatter_properties>&
+    properties_for_name(const yarn::name& n) const;
+
+    const settings::bundle& bundle_for_name(const yarn::name& n) const;
+
+    context make(const yarn::name& n) const;
+
+private:
+    const settings::bundle_repository& bundle_;
+    const settings::helper_settings_repository& helper_settings_;
+    const formattables::formatter_properties_repository& formatter_properties_;
+    const std::unordered_map<
+        std::string,
+        std::unordered_map<std::string
+                           , std::shared_ptr<formatter_helper_interface>>>&
+        formatter_helpers_;
+};
+
+} } } }
+
+#endif
---
+++ projects/quilt.cpp/src/types/formatters/context_factory.cpp
@@ -0,0 +1,85 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include <boost/throw_exception.hpp>
+#include "dogen/utility/log/logger.hpp"
+#include "dogen/quilt.cpp/types/formatters/building_error.hpp"
+#include "dogen/quilt.cpp/types/formatters/context_factory.hpp"
+
+namespace {
+
+using namespace dogen::utility::log;
+static logger lg(logger_factory("quit.cpp.formatters.context_factory"));
+
+const std::string formatter_properties_not_found(
+    "Could not find properties for formatter: ");
+const std::string bundle_not_found(
+    "Could not find settings bundle for: ");
+
+}
+
+namespace dogen {
+namespace quilt {
+namespace cpp {
+namespace formatters {
+
+context_factory::context_factory(const settings::bundle_repository& brp,
+    const settings::helper_settings_repository& hsrp,
+    const formattables::formatter_properties_repository& fprp,
+    const std::unordered_map<
+    std::string, std::unordered_map<
+    std::string,
+    std::shared_ptr<formatter_helper_interface>>>& helpers)
+    : bundle_(brp), helper_settings_(hsrp),
+      formatter_properties_(fprp), formatter_helpers_(helpers) {}
+
+const std::unordered_map<std::string, formattables::formatter_properties>&
+context_factory::properties_for_name(const yarn::name& n) const {
+    const auto& fp(formatter_properties_.formatter_properties_by_name());
+    const auto qn(n.qualified());
+    const auto i(fp.find(qn));
+    if (i == fp.end()) {
+        BOOST_LOG_SEV(lg, error) << formatter_properties_not_found << qn;
+        BOOST_THROW_EXCEPTION(
+            workflow_error(formatter_properties_not_found + qn));
+    }
+    return i->second;
+}
+
+const settings::bundle& context_factory::
+bundle_for_name(const yarn::name& n) const {
+    const auto& b(bundle_.bundles_by_name());
+    const auto qn(n.qualified());
+    const auto i(b.find(qn));
+    if (i == b.end()) {
+        BOOST_LOG_SEV(lg, error) << bundle_not_found << qn;
+        BOOST_THROW_EXCEPTION(workflow_error(bundle_not_found + qn));
+    }
+    return i->second;
+}
+
+context context_factory::make(const yarn::name& n) const {
+    const auto& fp(properties_for_name(n));
+    const auto& hs(helper_settings_.helper_settings_by_name());
+    const auto& b(bundle_for_name(n));
+    return context(b, hs, fp, formatter_helpers_);
+}
+
+} } } }
