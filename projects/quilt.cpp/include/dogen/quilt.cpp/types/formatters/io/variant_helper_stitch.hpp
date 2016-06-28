c0451f3cdf51e054bdc83962be7aa36c1a15dfc3
Marco Craveiro <marco.craveiro@gmail.com>
2016-06-28 22:33:12 +0100

    quilt cpp: implement io variant helper

--- projects/quilt.cpp/include/dogen/quilt.cpp/types/formatters/io/variant_helper_stitch.hpp
+++ projects/quilt.cpp/include/dogen/quilt.cpp/types/formatters/io/variant_helper_stitch.hpp
@@ -35,7 +35,7 @@ namespace cpp {
 namespace formatters {
 namespace io {
 
-class variant_helper_stitch : public formatter_helper_interface {
+class variant_helper : public formatter_helper_interface {
 public:
     std::string family() const;
     std::list<std::string> owning_formatters() const;
--- projects/quilt.cpp/src/types/formatters/io/variant_helper_stitch.cpp
+++ projects/quilt.cpp/src/types/formatters/io/variant_helper_stitch.cpp
@@ -18,6 +18,9 @@
  * MA 02110-1301, USA.
  *
  */
+#include "dogen/quilt.cpp/types/properties/helper_properties.hpp"
+#include "dogen/quilt.cpp/types/formatters/io/traits.hpp"
+#include "dogen/quilt.cpp/types/formatters/assistant.hpp"
 #include "dogen/quilt.cpp/types/formatters/io/variant_helper_stitch.hpp"
 
 namespace dogen {
@@ -25,6 +28,73 @@ namespace quilt {
 namespace cpp {
 namespace formatters {
 namespace io {
+
+std::string variant_helper::family() const {
+    static std::string r("SmartPointer");
+    return r;
+}
+
+std::list<std::string> variant_helper::owning_formatters() const {
+    static auto r(std::list<std::string> {
+        traits::class_implementation_formatter_name()
+    });
+    return r;
+}
+
+bool variant_helper::requires_explicit_call() const {
+    return false;
+}
+
+std::string variant_helper::function_name() const {
+    static std::string r("operator==");
+    return r;
+}
+
+bool variant_helper::
+is_enabled(const assistant& /*a*/, const bool /*in_inheritance*/) const {
+    return true;
+}
+
+void variant_helper::
+format(assistant& a, const properties::helper_properties& hp) const {
+    {
+        const auto d(hp.current());
+        const auto nt_qn(d.name_tree_qualified());
+        const auto n_qn(d.name_qualified());
+        auto snf(a.make_scoped_namespace_formatter(d.namespaces()));
+a.stream() << std::endl;
+a.stream() << "struct " << d.name_tree_identifiable() << "_visitor : public boost::static_visitor<> {" << std::endl;
+a.stream() << "    " << d.name_tree_identifiable() << "_visitor(std::ostream& s) : stream_(s) {" << std::endl;
+a.stream() << "        s << \"{ \" << \"\\\"__type__\\\": \" << \"\\\"" << d.name_qualified() << "\\\"\" << \", \";" << std::endl;
+a.stream() << "        s << \"\\\"data\\\": \";" << std::endl;
+a.stream() << "    }" << std::endl;
+a.stream() << std::endl;
+a.stream() << "    ~" << d.name_tree_identifiable() << "_visitor() { stream_ << \" }\"; }" << std::endl;
+        for (const auto& dd : hp.direct_descendants()) {
+a.stream() << std::endl;
+a.stream() << "    void operator()(const " << dd.name_qualified() << (dd.is_primitive() ? "" : "&") << " v) const {" << std::endl;
+            if (dd.is_primitive()) {
+a.stream() << "        stream_ << \"{ \" << \"\\\"__type__\\\": \" << \"\\\"" << dd.name_qualified() << "\\\"\" << \", \";" << std::endl;
+a.stream() << "        stream_ << \"\\\"value\\\": \";" << std::endl;
+a.stream() << "        stream_ << " << a.streaming_for_type(dd, "v") << ";" << std::endl;
+a.stream() << "        stream_ << \" }\";" << std::endl;
+            } else
+a.stream() << "        stream_ << " << a.streaming_for_type(dd, "v") << ";" << std::endl;
+a.stream() << "    }" << std::endl;
+        }
+a.stream() << std::endl;
+a.stream() << "private:" << std::endl;
+a.stream() << "    std::ostream& stream_;" << std::endl;
+a.stream() << "};" << std::endl;
+a.stream() << std::endl;
+a.stream() << "inline std::ostream& operator<<(std::ostream& s, const " << d.name_tree_qualified() << "& v) {" << std::endl;
+a.stream() << "    boost::apply_visitor(" << d.name_tree_identifiable() << "_visitor(s), v);" << std::endl;
+a.stream() << "    return s;" << std::endl;
+a.stream() << "}" << std::endl;
+a.stream() << std::endl;
+    }
+a.stream() << std::endl;
+}
 
 void variant_helper_stitch(
     nested_type_formatting_assistant& a,
