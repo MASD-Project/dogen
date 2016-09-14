42c9205e6e8e0a59de05b7102e94e493c6fb1888
Marco Craveiro <marco.craveiro@gmail.com>
2016-09-14 10:53:58 +0100

    quilt.cpp: generate path derivatives from formatters

--- projects/quilt.cpp/src/types/formatters/odb/enum_header_formatter.cpp
+++ projects/quilt.cpp/src/types/formatters/odb/enum_header_formatter.cpp
@@ -64,9 +64,9 @@ std::list<std::string> provider::provide_inclusion_dependencies(
 }
 
 properties::path_derivatives provider::provide_path_derivatives(
-    const properties::path_derivatives_factory& /*f*/,
-    const yarn::name& /*n*/) const {
-    properties::path_derivatives r;
+    const properties::path_derivatives_factory& f,
+    const yarn::name& n) const {
+    const auto r(f.make_cpp_header(n, formatter_name()));
     return r;
 }
 
