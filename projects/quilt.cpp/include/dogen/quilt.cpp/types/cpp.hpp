bdd19129b6f10be37eb54a26af074b541a36c5db
Marco Craveiro <marco.craveiro@gmail.com>
2016-09-23 23:01:52 +0100

    quilt.cpp: start work on transformer
    - remove comments on quilt.cpp as they have bit-rotted quite a lot.

--- projects/quilt.cpp/include/dogen/quilt.cpp/types/cpp.hpp
+++ projects/quilt.cpp/include/dogen/quilt.cpp/types/cpp.hpp
@@ -29,53 +29,7 @@ namespace dogen {
 namespace quilt {
 
 /**
- * @brief CPP is the C++ model.
- *
- * @section cpp_0 Objective
- *
- * CPP aims to model the types required by the formatters to generate C++ code.
- * Note that it is @e not a model of the C++ language as a compiler etc would
- * see it; it at a much higher level than an AST. In fact, one can think of CPP
- * as an yarn augmented with C++-specific information. Some of that information
- * comes from bringing yarn meta-data into the type system; the remaining is
- * directly derived by the information available in yarn.
- *
- * @section cpp_1 Info postfix
- *
- * The types in the CPP model should not have any prefixes or post-fixes since
- * they naturally belong to the model; e.g. @e cpp::class tells us everything we
- * need to know. However, names like @e class and @e namespace clash with
- * keywords,  so this is not a workable approach. Models such as ECore have
- * dealt with this problem by choosing a random prefix (e.g. @e EClass, etc).
- * We decided, somewhat arbitrarily, to use the postfix @e _info. It is used by
- * @e type_info, representing meta-data in C++, so at least that's one excuse.
- *
- * @section cpp_2 Other approaches
- *
- * We have tried to make C++ a model of the C++ language at the AST level and
- * the formatters nothing but a trivial dumper of the AST into file; in this
- * world we'd be consuming Clang libraries and using their infrastructure.
- * Whilst this approach was seductive in theorethical terms, it provided to be
- * to cumbersome to work in practice. This is because the AST is so low-level
- * that to describe a simple thing like a class would require large amounts
- * of infrastructure (think of it like talking about cars at the atomic level).
- *
- * We also tried to use yarn directly to format C++ code, augmented with
- * high-level meta-data. Since the C++ model shares so many commonalities with
- * yarn it only seeemed logical to remove it from the picture altogether and
- * add whatever was missing directly to yarn. To avoid making yarn language
- * specific, we added the meta-data as a non-typed container. Whilst workable,
- * this approach resulted in a lot of boilerplate code and little type safety.
- *
- * The final aproach considered was to make CPP types inherit from yarn. This
- * was also problematic: we do not support cross-model inheritance at present;
- * but much more worryingly, the model would become a complex inheritance
- * graph, with lots of double-dispatching, casting, abstract factories,
- * decorator patterns and all of the complexity that inevitable comes with
- * the heavy use of inheritance.
- *
- * This is why we settled on something which duplicates quite a bit of yarn; it
- * seems like the least bad approach.
+ * @brief CPP is the C++ model in the quilt kernel.
  *
  */
 namespace cpp {
