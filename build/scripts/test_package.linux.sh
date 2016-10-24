077136b1afe7b5e1cd3e36ff364410c228794524
Marco Craveiro <marco.craveiro@gmail.com>
2016-10-24 16:11:34 +0100

    script: improve package testing script

--- build/scripts/test_package.linux.sh
+++ build/scripts/test_package.linux.sh
@@ -17,6 +17,68 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 #
-sudo dpkg -i stage/pkg/dogen_*_amd64-applications.deb
+build_type="$1"
+shift
+if [[ "x${build_type}" = "x" ]]; then
+    build_type="Release";
+    echo "* Build type: ${build_type} (default)"
+else
+    echo "* Build type: ${build_type}"
+fi
+
+#
+# Root directory for the product.
+#
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+product_dir=$(readlink -f ${dir}/../..)
+echo "* Product directory: ${product_dir}"
+
+#
+# Compiler
+#
+compiler="$1"
+shift
+if [[ "x${compiler}" = "x" ]]; then
+    compiler="gcc";
+    echo "* Compiler: ${compiler} (default)"
+    export CC=gcc-6
+    export CXX=g++-6
+elif [ "${compiler}" = "gcc" ]; then
+    echo "* Compiler: ${compiler}"
+    export CC=gcc-6
+    export CXX=g++-6
+elif [ "${compiler}" = "clang" ]; then
+    echo "* Compiler: ${compiler}"
+    export CC=clang-3.7
+    export CXX=clang++-3.7
+else
+    echo "* Unrecognised compiler: ${compiler}"
+    exit
+fi
+
+#
+# Setup directories
+#
+output_dir="${product_dir}/build/output";
+if [[ ! -e $output_dir ]]; then
+    mkdir $output_dir
+fi
+
+compiler_dir="${output_dir}/${compiler}";
+if [[ ! -e $compiler_dir ]]; then
+    mkdir $compiler_dir
+fi
+
+build_type_dir="${compiler_dir}/${build_type}";
+if [[ ! -e $build_type_dir ]]; then
+    mkdir $build_type_dir
+fi
+
+#
+# Test the package
+#
+sudo dpkg -i ${build_type_dir}/stage/pkg/dogen_*_amd64-applications.deb
+cp ${product_dir}/diagrams/hello_world.dia /tmp
 cd /tmp
 /usr/bin/dogen.knitter --version
+/usr/bin/dogen.knitter -t hello_world.dia
