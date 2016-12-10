84eb75f2698b57bef95698bf153e118c66d42d53
Marco Craveiro <marco.craveiro@gmail.com>
2016-12-10 17:57:09 +0000

    quilt.csharp: add scenarios for no property and one property

---
+++ projects/test_models/CSharpModel/Types/OneProperty.cs
@@ -0,0 +1,63 @@
+// -*- mode: csharp; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+//
+// Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+//
+namespace Dogen.TestModels.CSharpModel
+{
+    /// <summary>
+    /// This class tests the use case of one property.
+    /// </summary>
+    class OneProperty
+    {
+        #region Properties
+        /// <summary>
+        /// Just the one property.
+        /// </summary>
+        public int Property { get; set; }
+        #endregion
+
+        #region Equality
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+
+            var value = obj as OneProperty;
+            if (value == null) return false;
+
+            return
+                Property == value.Property;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                // Choose large primes to avoid hashing collisions
+                const int HashingBase = (int) 2166136261;
+                const int HashingMultiplier = 16777619;
+
+                int hash = HashingBase;
+                hash = (hash * HashingMultiplier) ^ Property.GetHashCode();
+                return hash;
+            }
+       }
+       #endregion
+    }
+}
