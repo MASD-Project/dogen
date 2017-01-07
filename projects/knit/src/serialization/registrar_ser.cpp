50ff9393cdc25f6539bab18b107555baaa1146d3
Marco Craveiro <marco.craveiro@gmail.com>
2017-01-07 18:38:45 +0000

    regenerate all models with latest knitter
    - registrar oscillator

--- projects/knit/src/serialization/registrar_ser.cpp
+++ projects/knit/src/serialization/registrar_ser.cpp
@@ -38,11 +38,11 @@ namespace knit {
 
 template<typename Archive>
 void register_types(Archive& ar) {
+    dogen::quilt::cpp::register_types(ar);
+    dogen::annotations::register_types(ar);
     dogen::yarn::register_types(ar);
     dogen::formatters::register_types(ar);
     dogen::options::register_types(ar);
-    dogen::annotations::register_types(ar);
-    dogen::quilt::cpp::register_types(ar);
 }
 
 template void register_types(boost::archive::polymorphic_oarchive& ar);
