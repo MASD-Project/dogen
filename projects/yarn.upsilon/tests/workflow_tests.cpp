62b771b81de65a4de2747650d7821f87dbf57b75
Marco Craveiro <marco.craveiro@gmail.com>
2017-01-15 00:14:53 +0000

    yarn.upsilon: add yarn test model skeleton to test

--- projects/yarn.upsilon/tests/workflow_tests.cpp
+++ projects/yarn.upsilon/tests/workflow_tests.cpp
@@ -18,17 +18,45 @@
  * MA 02110-1301, USA.
  *
  */
+#include <istream>
+#include <fstream>
+#include <sstream>
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
 #include "dogen/utility/test/logging.hpp"
+#include "dogen/utility/test/asserter.hpp"
 #include "dogen/utility/test_data/yarn_upsilon.hpp"
+#include "dogen/utility/test/xml_serialization_helper.hpp"
+#include "dogen/annotations/test/mock_type_repository_factory.hpp"
+#include "dogen/upsilon/serialization/registrar_ser.hpp"
+#include "dogen/yarn/serialization/registrar_ser.hpp"
 #include "dogen/yarn/types/intermediate_model.hpp"
 #include "dogen/yarn/io/intermediate_model_io.hpp"
+#include "dogen/yarn/serialization/intermediate_model_ser.hpp"
+#include "dogen/yarn.upsilon/types/workflow.hpp"
+#include "dogen/upsilon/serialization/model_ser.hpp"
+
+template<typename Archive> void register_types(Archive& ar) {
+    dogen::yarn::register_types<Archive>(ar);
+}
+
+using dogen::utility::test::asserter;
 
 namespace {
 
 const std::string empty;
 const std::string test_module("yarn.upsilon");
 const std::string test_suite("workflow_tests");
+
+dogen::upsilon::model from_xml(std::istream& s) {
+    boost::archive::xml_iarchive a(s);
+    dogen::upsilon::register_types(a);
+    dogen::upsilon::model r;
+    a & boost::serialization::make_nvp("model", r);
+    return r;
+}
 
 }
 
@@ -37,10 +65,24 @@ BOOST_AUTO_TEST_SUITE(workflow_tests)
 BOOST_AUTO_TEST_CASE(test_data_mode_transforms_into_expected_yarn) {
     SETUP_TEST_LOG("test_data_mode_transforms_into_expected_yarn");
 
-    // using dogen::utility::test_data::yarn_upsilon;
-    // const auto input(yarn_upsilon::input_test_model_configuration_xml());
-    // dogen::yarn::upsilon::
+    using dogen::utility::test_data::yarn_upsilon;
+    const auto input_path(yarn_upsilon::expected_test_model_upsilon_xml());
 
+    boost::filesystem::ifstream s(input_path);
+    const auto input(from_xml(s));
+
+    const std::string model_name(input_path.stem().string());
+
+    using namespace dogen::annotations::test;
+    mock_type_repository_factory rf;
+    const auto rp(rf.make());
+    dogen::yarn::upsilon::workflow w;
+    dogen::yarn::intermediate_model actual(w.execute(input));
+
+    const auto expected_path(yarn_upsilon::expected_test_model_yarn_xml());
+    const auto actual_path(yarn_upsilon::actual_test_model_yarn_xml());
+
+    BOOST_CHECK(asserter::assert_object(expected_path, actual_path, actual));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
