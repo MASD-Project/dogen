1bc5df4d1c7c9bf6a437f0c50c07e6472670cb99
Marco Craveiro <marco.craveiro@gmail.com>
2017-01-23 15:30:54 +0000

    regenerate all models with latest knitter
    - pick up backslash escaping.

--- projects/quilt.csharp/src/io/formattables/element_properties_io.cpp
+++ projects/quilt.csharp/src/io/formattables/element_properties_io.cpp
@@ -45,6 +45,7 @@ inline std::string tidy_up_string(std::string s) {
     boost::replace_all(s, "\r\n", "<new_line>");
     boost::replace_all(s, "\n", "<new_line>");
     boost::replace_all(s, "\"", "<quote>");
+    boost::replace_all(s, "\\", "<backslash>");
     return s;
 }
 
