641b4bd4d1e5a5a727c912a874f2eacc7677b195
Marco Craveiro <marco.craveiro@gmail.com>
2017-03-17 11:29:59 +0000

    scripts: remove the extra slash

--- build/scripts/appveyor.install.windows.ps1
+++ build/scripts/appveyor.install.windows.ps1
@@ -17,7 +17,7 @@
 # MA 02110-1301, USA.
 #
 
-$dropbox="https://www.dropbox.com/s/ntz6moq7kg9a8m7/"
+$dropbox="https://www.dropbox.com/s/ntz6moq7kg9a8m7"
 $version=4
 $package="dogen_deps_vc14_windows_amd64_v${version}.7z"
 $input_location="${dropbox}/${package}"
