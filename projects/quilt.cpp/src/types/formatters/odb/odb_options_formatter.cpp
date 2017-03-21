3055ab768de7bd1b4dd2fa7863b31ebcec5876d7
Marco Craveiro <marco.craveiro@gmail.com>
2017-03-21 12:09:44 +0000

    quilt.cpp: do not add multi-database and common to odb options for a single database

--- projects/quilt.cpp/src/types/formatters/odb/odb_options_formatter.cpp
+++ projects/quilt.cpp/src/types/formatters/odb/odb_options_formatter.cpp
@@ -112,7 +112,7 @@ a.stream() << "--sql-name-case " << a.get_letter_case(*o.letter_case()) << std::
 a.stream() << std::endl;
         }
 
-        if (!o.databases().empty()) {
+        if (o.databases().size() > 1) {
 a.stream() << "# enable multi-database support" << std::endl;
 a.stream() << "--multi-database static" << std::endl;
 a.stream() << std::endl;
@@ -120,6 +120,7 @@ a.stream() << std::endl;
 
         if (!o.databases().empty()) {
 a.stream() << "# target databases" << std::endl;
+            if (o.databases().size() > 1)
 a.stream() << "--database common" << std::endl;
 
             for (const auto& d : o.databases())
