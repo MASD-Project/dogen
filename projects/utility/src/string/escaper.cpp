a2e353a87a816766546ab681571ae222610b7aa9
Marco Craveiro <marco.craveiro@gmail.com>
2017-09-08 14:31:51 +0100

    utility: add class to escape strings

---
+++ projects/utility/src/string/escaper.cpp
@@ -0,0 +1,48 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include <boost/algorithm/string.hpp>
+#include "dogen/utility/log/logger.hpp"
+#include "dogen/utility/string/escaper.hpp"
+
+namespace {
+
+using namespace dogen::utility::log;
+auto lg(logger_factory("utility.string.escaper"));
+
+}
+
+namespace dogen {
+namespace utility {
+namespace string {
+
+void escaper::escape(std::string& s) {
+    boost::replace_all(s, "\\", "\\\\");
+    boost::replace_all(s, "\r\n", "\\r\\n");
+    boost::replace_all(s, "\n", "\\n");
+    boost::replace_all(s, "\"", "\\\"");
+}
+
+std::string escaper::escape_copy(std::string s) {
+    escape(s);
+    return s;
+}
+
+} } }
