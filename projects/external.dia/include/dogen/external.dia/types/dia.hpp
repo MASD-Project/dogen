aa1ff4c4cab15f327f146fd57ef4f98b3ac51c51
Marco Craveiro <marco.craveiro@gmail.com>
2017-12-10 21:15:12 +0000

    input models: clean up external.dia model

--- projects/external.dia/include/dogen/external.dia/types/dia.hpp
+++ projects/external.dia/include/dogen/external.dia/types/dia.hpp
@@ -29,16 +29,17 @@ namespace dogen {
 namespace external {
 
 /**
- * @brief Yarn Dia is a unidirectional transformation model that transforms
- * Dia @e diagram into an yarn @e model.
+ * @brief External.Dia is a unidirectional transformation model that transforms
+ * Dia @e diagram into an external @e model.
  *
  * By that we mean that it is a model designed to transform a representation of
- * a user model in Dia diagram format to its counterpart in yarn format, taking
- * into account any special yarn meta-data which the user may have encoded into
- * the Dia diagram. Note that the Dia diagram format contains a representation
- * of UML.
+ * a user model in Dia diagram format to its counterpart in the external format,
+ * taking into account any special yarn meta-data which the user may have encoded
+ * into the Dia diagram. Note that the Dia diagram format contains a representation
+ * of UML. We pass that meta-data straight through into the external model, without
+ * any further processing.
  *
- * @section yarn_dia_0 Dia diagrams as DAGs
+ * @section external_dia_0 Dia diagrams as DAGs
  *
  * Dia diagrams are made up of @e objects, identified by IDs. These objects
  * contain a number of UML model elements, which we are interested in. For
@@ -48,27 +49,26 @@ namespace external {
  * @li UML generalisation
  * @li UML notes
  *
- * and so on. Each of these objects will contribute to a yarn object; but note
- * that the mapping is not one-to-one.For example, in order to process an
- * yarn object, one needs to have both the UML class as well as any
- * generalisations it may be involved in (both as a child or as a parent). To
- * make our life easier we created a DAG of Dia objects that provides us with
- * the information in dependency order. That is, we process dia objects in
- * such a way that when a dependency exists, we know that the object in which
- * we depend on has already been processed. Please note that to ensure no
+ * and so on. Each of these objects will contribute to am external element.
+ * However, we need to be aware of object containment - e.g. if a class is
+ * contained in a package. This is because the external element names are scoped
+ * by the packages. So, to make our life easier we created a DAG of Dia objects
+ * that provides us with the information in dependency order. That is, we process
+ * dia objects in such a way that when a dependency exists, we know that the object
+ * in which we depend on has already been processed. Please note that to ensure no
  * cycles we had to ignore some of the UML objects such as UML aggregation -
  * but fortunately, these objects do not provide sufficient information for
- * an yarn transformation anyway so not much was lost (instead, one must
+ * an external transformation anyway so not much was lost (instead, one must
  * manually declare attributes in a class).
  *
  * See @ref grapher for details on how the the DAG is built.
  *
- * @section yarn_dia_1 Intermediate processing
+ * @section external_dia_1 Intermediate processing
  *
  * To make the code easier to read and maintain, we created a numeber of
  * intermediate stages between a Dia diagram and yarn:
  *
- * @li Dia object -> processed object -> profile -> Yarn
+ * @li Dia object -> processed object -> profile -> external element
  *
  * This may look a bit excessive at first glance, but it was done because
  * the shape of the Dia objects is a bit too generic for our purposes, and
@@ -81,7 +81,7 @@ namespace external {
  * create the corresponding yarn entity, and the transformation code is
  * not obscured.
  *
- * @section yarn_dia_2 Dia meta-data
+ * @section external_dia_2 Dia meta-data
  *
  * There are a couple of conventions on how to model in Dia that one
  * needs to follow in order to comply with Dogen:
@@ -90,15 +90,11 @@ namespace external {
  * yarn property rather than a member variable. This means that it is
  * expected that a getter and a setter will be generated.
  *
- * @li a number of DDD stereotypes were added:@e service, @e value and @e
- * object. These all map directly to the DDD ideas defined by Eric Evans
- * in the Domain Driven Design book.
+ * @li a number of stereotypes were added, as per the definitions in the
+ * UML profile. For details, see the Dogen UML profile.
  *
- * @li a number of yarn specific stereotypes were added: @e exception,
- * @e enumeration, @e object template, @e visitable, @e fluent, @e immutable,
- * @e comparable. These we describe below.
- *
- * @li any type without any stereotype defaults to @e value @e object.
+ * @li any type without any stereotype assumes a default stereotype, again
+ * as per Dogen UML profile.
  *
  * @li any UML Note with the marker #DOGEN yarn.dia.comment=true will be
  * used as the documentation of the current package - or the model, if
@@ -126,8 +122,6 @@ namespace external {
  * @li @b fluent: a fluent interface will be generated for all properties.
  *
  * @li @b immutable: only getters will be generated for each property.
- *
- * @li @b comparable: unused as of yet.
  */
 namespace dia {
 }
