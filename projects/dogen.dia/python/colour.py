80a587be38eb88a40bcabc9cb87ea16f61742fb3
Marco Craveiro <marco.craveiro@gmail.com>
2018-10-11 20:27:35 +0100

    dia: update colour of primitives in diagrams

--- projects/dogen.dia/python/colour.py
+++ projects/dogen.dia/python/colour.py
@@ -35,7 +35,7 @@ for layer in dia.active_display().diagram.data.layers:
         elif "dogen::enumeration" in stereotype:
             object.properties["fill_colour"] = "Papaya Whip"
         elif "dogen::primitive" in stereotype:
-            object.properties["fill_colour"] = "Light Goldenrod Yellow"
+            object.properties["fill_colour"] = "Light Steel Blue"
         elif "dogen::exception" in stereotype:
             object.properties["fill_colour"] = "Peach Puff"
         elif "dogen::object_template" in stereotype:
