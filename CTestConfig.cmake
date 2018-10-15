e5cc8f7c92237a9633d91ffba836d5fc5263aa4d
Marco Craveiro <marco.craveiro@gmail.com>
2018-10-15 08:27:39 +0100

    ctest: update to new project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -18,8 +18,8 @@
 # MA 02110-1301, USA.
 #
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
-set(CTEST_PROJECT_NAME "dogen")
+set(CTEST_PROJECT_NAME "MASD Project - Dogen")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=MASD+Project")
+set(CTEST_DROP_LOCATION "/submit.php?project=MASD+Project+-+Dogen")
 set(CTEST_DROP_SITE_CDASH TRUE)
