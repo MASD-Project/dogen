54cf6e2aab0cfe26b30646b5a44ff06761d50d39
Marco Craveiro <marco.craveiro@gmail.com>
2018-10-24 18:37:42 +0100

    scripts: update package tests to new project prefix

--- build/scripts/test_package.linux.sh
+++ build/scripts/test_package.linux.sh
@@ -63,5 +63,5 @@ build_type_dir="${compiler_dir}/${build_type}";
 sudo dpkg -i ${build_type_dir}/stage/pkg/dogen_*_amd64-applications.deb
 cp ${product_dir}/projects/dogen.models/dia/hello_world.dia /tmp
 cd /tmp
-/usr/bin/dogen.knitter --version
-/usr/bin/dogen.knitter -t hello_world.dia
+/usr/bin/masd.dogen.knitter --version
+/usr/bin/masd.dogen.knitter -t hello_world.dia
