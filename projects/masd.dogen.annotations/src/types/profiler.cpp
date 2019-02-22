f9916c2c6517c22f307fabc81a9f48d18d30b9da
Marco Craveiro <marco.craveiro@gmail.com>
2019-02-22 18:54:53 +0000

    move decoration related fields into masd namespace

--- projects/masd.dogen.annotations/src/types/profiler.cpp
+++ projects/masd.dogen.annotations/src/types/profiler.cpp
@@ -201,7 +201,11 @@ void profiler::setup_annotations(const archetype_location_repository& alrp,
                  * for example, a profile for knitter may not work for
                  * stitch, because its missing all of the
                  * formatters. This is not a very elegant solution,
-                 * but it will do until we think of something better.
+                 * but it will do until we think of something
+                 * better. Note that this only happens for stitch
+                 * tests at present (because there we do not want a
+                 * dependency on the rest of the code base). FIXME:
+                 * this is a hack that needs to be removed.
                  */
                 BOOST_LOG_SEV(lg, error) << "Error instantiating profile: "
                                          << prfn << ". Message: " << e.what()
