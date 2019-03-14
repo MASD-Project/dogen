510877e243a1dcbaae2fa7dbb732935bef983bfb
Marco Craveiro <marco.craveiro@gmail.com>
2019-03-14 13:51:06 +0000

    orchestration: add test for deleting empty folders on

--- projects/masd.dogen.orchestration/tests/code_generation_chain_tests.cpp
+++ projects/masd.dogen.orchestration/tests/code_generation_chain_tests.cpp
@@ -38,7 +38,10 @@ const std::string test_module("masd.dogen.orchestration.tests");
 const std::string test_suite("code_generation_chain_tests");
 
 const std::string run_activity("code_generation");
-
+const std::string first_folder("first");
+const std::string second_folder("second");
+const std::string first_folders_path("first/second/third");
+const std::string second_folders_path("src/second");
 
 /*
  * Set these flag to true if you want to dump information for all
@@ -60,9 +63,10 @@ bool are_generated_files_healthy(const boost::filesystem::path& output_dir,
     /*
      * Ensure we have created a model directory.
      */
+    using namespace boost::filesystem;
     auto n(target.filename().stem());
     const auto dir(output_dir / n);
-    if (!boost::filesystem::exists(dir))
+    if (!exists(dir))
         return false;
 
     /*
@@ -78,7 +82,7 @@ bool are_generated_files_healthy(const boost::filesystem::path& output_dir,
      */
     const boost::uintmax_t minimum_file_size(1000);
     for (const auto& f : files) {
-        const auto sz(boost::filesystem::file_size(f));
+        const auto sz(file_size(f));
         if (sz < minimum_file_size)
             return false;
     }
@@ -91,7 +95,7 @@ bool are_generated_files_healthy(const boost::filesystem::path& output_dir,
  * @note the output directory is recreated on initialisation so we
  * don't need to worry about its state.
  */
-void test_code_generation(const boost::filesystem::path& target,
+void execute_code_generation_transform(const boost::filesystem::path& target,
     const boost::filesystem::path& output_dir,
     const bool enable_tracing_locally = false,
     const bool enable_reporting_locally = false,
@@ -130,7 +134,7 @@ BOOST_AUTO_TEST_CASE(masd_dogen_annotations_dia_produces_expected_code) {
     using masd::dogen::utility::test_data::dogen_generation;
     const auto t(dogen_generation::input_masd_dogen_annotations_dia());
     const auto od(dogen_generation::output_directory());
-    test_code_generation(t, od);
+    execute_code_generation_transform(t, od);
     BOOST_CHECK(are_generated_files_healthy(od, t));
 }
 
@@ -139,8 +143,38 @@ BOOST_AUTO_TEST_CASE(masd_dogen_coding_json_produces_expected_model) {
     using masd::dogen::utility::test_data::dogen_generation;
     const auto t(dogen_generation::input_masd_dogen_coding_json());
     const auto od(dogen_generation::output_directory());
-    test_code_generation(t, od);
+    execute_code_generation_transform(t, od);
     BOOST_CHECK(are_generated_files_healthy(od, t));
+}
+
+BOOST_AUTO_TEST_CASE(empty_folders_are_deleted_when_delete_empty_folders_flag_is_on) {
+    SETUP_TEST_LOG("empty_folders_are_deleted_when_delete_empty_folders_flag_is_on");
+
+    using masd::dogen::utility::test_data::dogen_generation;
+    const auto t(dogen_generation::input_masd_dogen_dia());
+    const auto od(dogen_generation::output_directory());
+    execute_code_generation_transform(t, od);
+    BOOST_REQUIRE(are_generated_files_healthy(od, t, 60/*minimum_number*/));
+
+    auto n(t.filename().stem());
+    const auto model_dir(od / n);
+
+    using namespace boost::filesystem;
+    const path first_empty_folders(model_dir / first_folders_path);
+    create_directories(first_empty_folders);
+    BOOST_CHECK(exists(first_empty_folders));
+
+    const path second_empty_folders(model_dir / second_folders_path);
+    create_directories(second_empty_folders);
+    BOOST_CHECK(exists(second_empty_folders));
+
+    execute_code_generation_transform(t, od);
+
+    BOOST_CHECK(!exists(first_empty_folders));
+    BOOST_CHECK(!exists(second_empty_folders));
+
+    const path first_empty_folder(model_dir / first_folder);
+    BOOST_CHECK(!exists(first_empty_folder));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
