44c33cebb64e75154a380c79f09a44582ce0cb75
Marco Craveiro <marco.craveiro@gmail.com>
2019-03-16 07:31:36 +0000

    generation.cpp: improvements to tests
    - regenerate masd.dogen to pickup new tests

--- projects/masd.dogen/tests/configuration_tests.cpp
+++ projects/masd.dogen/tests/configuration_tests.cpp
@@ -52,4 +52,55 @@ BOOST_AUTO_TEST_CASE(distinct_objects_are_unequal) {
     BOOST_CHECK(a != b);
 }
 
+BOOST_AUTO_TEST_CASE(assigning_an_object_to_itself_results_in_the_same_object) {
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wself-assign-overloaded"
+#endif
+    masd::dogen::configuration_generator g;
+    auto a(g());
+    const auto b(a);
+    const auto c(g());
+
+    a = a;
+    BOOST_CHECK(a == b);
+    BOOST_CHECK(a != c);
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+}
+
+BOOST_AUTO_TEST_CASE(moved_objects_are_equal) {
+    masd::dogen::configuration_generator g;
+    auto a(g());
+    const auto b = masd::dogen::configuration();
+    const auto c(a);
+    BOOST_CHECK(a != b);
+    BOOST_CHECK(a == c);
+
+    const auto d = std::move(a);
+    BOOST_CHECK(d == c);
+    BOOST_CHECK(d != b);
+}
+
+BOOST_AUTO_TEST_CASE(assigned_objects_are_equal) {
+    masd::dogen::configuration_generator g;
+    const auto a(g());
+    masd::dogen::configuration b;
+    BOOST_CHECK(a != b);
+
+    b = a;
+    BOOST_CHECK(a == b);
+}
+
+BOOST_AUTO_TEST_CASE(copy_constructed_objects_are_equal) {
+    masd::dogen::configuration_generator g;
+    const auto a(g());
+    auto b(a);
+    BOOST_CHECK(a == b);
+
+    b = masd::dogen::configuration();
+    BOOST_CHECK(a != b);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
