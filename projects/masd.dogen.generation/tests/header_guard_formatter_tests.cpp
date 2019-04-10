b5940877e71ca0bf6bc4f5e07dc22572c4e31040
Marco Craveiro <marco.craveiro@gmail.com>
2019-04-10 16:00:07 +0100

    generation: reactivate header guard tests

--- projects/masd.dogen.generation/tests/header_guard_formatter_tests.cpp
+++ projects/masd.dogen.generation/tests/header_guard_formatter_tests.cpp
@@ -18,71 +18,73 @@
  * MA 02110-1301, USA.
  *
  */
-// #include <boost/test/unit_test.hpp>
-// #include "masd.dogen.extraction/types/indent_filter.hpp"
-// #include "masd.dogen.utility/types/test/asserter.hpp"
-// #include "masd.dogen.utility/types/test/logging.hpp"
-// #include "masd.dogen.utility/types/io/list_io.hpp"
-// #include "masd.dogen.utility/types/io/unordered_map_io.hpp"
-// #include "masd.dogen.extraction/types/cpp/header_guard_formatter.hpp"
+#include <boost/test/unit_test.hpp>
+#include "masd.dogen.utility/types/test/asserter.hpp"
+#include "masd.dogen.utility/types/test/logging.hpp"
+#include "masd.dogen.utility/types/io/list_io.hpp"
+#include "masd.dogen.utility/types/io/unordered_map_io.hpp"
+#include "masd.dogen.generation/types/formatters/indent_filter.hpp"
+#include "masd.dogen.generation/types/formatters/header_guard_formatter.hpp"
 
-// namespace {
+namespace {
 
-// const std::string test_module("masd.dogen.extraction.tests");
-// const std::string test_suite("header_guard_formatter_tests");
-// const std::string empty;
-// const std::string empty_guard;
-// const std::string non_empty_guard("A_FILE_HPP");
+const std::string test_module("masd.dogen.generation.tests");
+const std::string test_suite("header_guard_formatter_tests");
+const std::string empty;
+const std::string empty_guard;
+const std::string non_empty_guard("A_FILE_HPP");
 
-// const std::string with_guard(R"(#ifndef A_FILE_HPP
-// #define A_FILE_HPP
+const std::string with_guard(R"(#ifndef A_FILE_HPP
+#define A_FILE_HPP
 
-// #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-// #pragma once
-// #endif
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
 
-// #endif
-// )");
+#endif
+)");
 
-// }
+}
 
-// using namespace masd::dogen::utility::test;
-// using masd::dogen::utility::test::asserter;
+using masd::dogen::generation::formatters::indent_filter;
+using masd::dogen::generation::formatters::header_guard_formatter;
+using namespace masd::dogen::utility::test;
+using masd::dogen::utility::test::asserter;
 
-// BOOST_AUTO_TEST_SUITE(header_guard_formatter_tests)
+BOOST_AUTO_TEST_SUITE(header_guard_formatter_tests)
 
-// BOOST_IGNORE_AUTO_TEST_CASE(non_empty_path_produces_expected_header_guards) {
-//     SETUP_TEST_LOG_SOURCE("non_empty_path_produces_expected_header_guards");
-//     BOOST_LOG_SEV(lg, debug) << "Disable modeline top";
+BOOST_AUTO_TEST_CASE(non_empty_path_produces_expected_header_guards) {
+    SETUP_TEST_LOG_SOURCE("non_empty_path_produces_expected_header_guards");
+    BOOST_LOG_SEV(lg, debug) << "Disable modeline top";
 
-//     std::ostringstream s;
-//     boost::iostreams::filtering_ostream fo;
-//     masd::dogen::extraction::indent_filter::push(fo, 4);
-//     fo.push(s);
+    std::ostringstream s;
+    boost::iostreams::filtering_ostream fo;
+    indent_filter::push(fo, 4);
+    fo.push(s);
 
-//     masd::dogen::extraction::cpp::header_guard_formatter f;
-//     f.format_begin(fo, non_empty_guard);
-//     f.format_end(fo, non_empty_guard);
-//     const auto r(s.str());
-//     BOOST_CHECK(asserter::assert_equals_string(with_guard, r));
-//     BOOST_LOG_SEV(lg, debug) << "Disable modeline bottom";
-// }
+    header_guard_formatter f(fo, non_empty_guard);
+    f.format_begin();
+    f.format_end();
+    const auto r(s.str());
+    BOOST_CHECK(asserter::assert_equals_string(with_guard, r));
+    BOOST_LOG_SEV(lg, debug) << "Disable modeline bottom";
+}
 
-// BOOST_IGNORE_AUTO_TEST_CASE(empty_path_produces_no_header_guards) {
-//     SETUP_TEST_LOG_SOURCE("empty_path_produces_no_header_guards");
-//     BOOST_LOG_SEV(lg, debug) << "Disable modeline top";
+BOOST_AUTO_TEST_CASE(empty_path_produces_no_header_guards) {
+    SETUP_TEST_LOG_SOURCE("empty_path_produces_no_header_guards");
+    BOOST_LOG_SEV(lg, debug) << "Disable modeline top";
 
-//     std::ostringstream s;
-//     boost::iostreams::filtering_ostream fo;
-//     masd::dogen::extraction::indent_filter::push(fo, 4);
-//     fo.push(s);
+    std::ostringstream s;
+    boost::iostreams::filtering_ostream fo;
+    indent_filter::push(fo, 4);
+    fo.push(s);
 
-//     masd::dogen::extraction::cpp::header_guard_formatter f;
-//     f.format_begin(s, empty_guard);
-//     f.format_end(s, empty_guard);
-//     const auto r(s.str());
-//     BOOST_CHECK(asserter::assert_equals_string(empty, r));
-//     BOOST_LOG_SEV(lg, debug) << "Disable modeline bottom";
-// }
+    header_guard_formatter f(s, empty_guard);
+    f.format_begin();
+    f.format_end();
+    const auto r(s.str());
+    BOOST_CHECK(asserter::assert_equals_string(empty, r));
+    BOOST_LOG_SEV(lg, debug) << "Disable modeline bottom";
+}
 
-// BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
