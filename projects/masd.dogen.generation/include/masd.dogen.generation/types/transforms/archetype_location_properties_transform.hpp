a5da4e61968c7757c7d736291777555b8328f4f8
Marco Craveiro <marco.craveiro@gmail.com>
2019-04-29 12:05:34 +0100

    generation: remove annotations from archetype location transform

--- projects/masd.dogen.generation/include/masd.dogen.generation/types/transforms/archetype_location_properties_transform.hpp
+++ projects/masd.dogen.generation/include/masd.dogen.generation/types/transforms/archetype_location_properties_transform.hpp
@@ -27,7 +27,6 @@
 
 #include <string>
 #include <boost/optional.hpp>
-#include "masd.dogen.variability/types/type.hpp"
 #include "masd.dogen.variability/types/meta_model/feature.hpp"
 #include "masd.dogen.variability/types/meta_model/configuration.hpp"
 #include "masd.dogen.variability/types/meta_model/feature_model.hpp"
@@ -44,94 +43,6 @@
 namespace masd::dogen::generation::transforms {
 
 class archetype_location_properties_transform final {
-private:
-    struct backend_type_group {
-        variability::type enabled;
-        variability::type directory;
-    };
-
-    friend std::ostream&
-    operator<<(std::ostream& s, const backend_type_group& v);
-
-    struct facet_type_group {
-        variability::type enabled;
-        variability::type overwrite;
-        boost::optional<variability::type> directory;
-        boost::optional<variability::type> postfix;
-    };
-
-    friend std::ostream& operator<<(std::ostream& s, const facet_type_group& v);
-
-    struct global_archetype_type_group {
-        variability::type enabled;
-        variability::type overwrite;
-        variability::type postfix;
-    };
-
-    friend std::ostream&
-    operator<<(std::ostream& s, const global_archetype_type_group& v);
-
-    struct local_archetype_type_group {
-        variability::type facet_enabled;
-        variability::type archetype_enabled;
-        variability::type facet_overwrite;
-        variability::type archetype_overwrite;
-    };
-
-    friend std::ostream&
-    operator<<(std::ostream& s, const local_archetype_type_group& v);
-
-private:
-    static std::unordered_map<std::string, backend_type_group>
-    make_backend_type_group(const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp);
-
-    static std::unordered_map<std::string, facet_type_group>
-    make_facet_type_group(const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp);
-
-    static std::unordered_map<std::string, global_archetype_type_group>
-    make_global_archetype_type_group(const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp);
-
-    static std::unordered_map<std::string, local_archetype_type_group>
-    make_local_archetype_type_group(const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp);
-
-private:
-    static std::unordered_map<std::string, meta_model::backend_properties>
-    obtain_backend_properties(
-        const std::unordered_map<std::string, backend_type_group>& tgs,
-        const variability::annotation& ra);
-
-    static std::unordered_map<std::string, meta_model::facet_properties>
-    obtain_facet_properties(
-        const std::unordered_map<std::string, facet_type_group>& tgs,
-        const variability::annotation& ra);
-
-    static std::unordered_map<std::string, meta_model::archetype_properties>
-    obtain_archetype_properties(
-        const std::unordered_map<std::string, global_archetype_type_group>& tgs,
-        const variability::annotation& ra);
-
-    static void populate_global_archetype_location_properties(
-        const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp,
-        meta_model::model& m);
-
-    static std::unordered_map<
-        std::string,
-        coding::meta_model::local_archetype_location_properties>
-    obtain_local_archetype_location_properties(
-        const std::unordered_map<std::string, local_archetype_type_group>& tgs,
-        const std::list<archetypes::location>& als,
-        const variability::annotation& a);
-
-    static void populate_local_archetype_location_properties(
-        const variability::type_repository& atrp,
-        const archetypes::location_repository& alrp,
-        meta_model::model& m);
-
 private:
     struct backend_feature_group {
         variability::meta_model::feature enabled;
