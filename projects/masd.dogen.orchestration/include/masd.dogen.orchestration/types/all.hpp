2c44eee7c3e7bb405eb46bd87ba153851f795d1c
Marco Craveiro <marco.craveiro@gmail.com>
2019-04-30 11:11:19 +0100

    orchestration: generate profile template adaption transform

--- projects/masd.dogen.orchestration/include/masd.dogen.orchestration/types/all.hpp
+++ projects/masd.dogen.orchestration/include/masd.dogen.orchestration/types/all.hpp
@@ -43,6 +43,7 @@
 #include "masd.dogen.orchestration/types/helpers/stereotypes_conversion_result.hpp"
 #include "masd.dogen.orchestration/types/transforms/profile_repository_transform.hpp"
 #include "masd.dogen.orchestration/types/transforms/extraction_model_production_chain.hpp"
+#include "masd.dogen.orchestration/types/transforms/profile_template_adaption_transform.hpp"
 #include "masd.dogen.orchestration/types/transforms/injection_model_to_coding_model_transform.hpp"
 #include "masd.dogen.orchestration/types/transforms/coding_model_to_generation_model_transform.hpp"
 #include "masd.dogen.orchestration/types/transforms/injection_model_set_to_coding_model_set_chain.hpp"
