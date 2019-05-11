b431c89c3fc0af86c1611f3e4ba52d068181126e
Marco Craveiro <marco.craveiro@gmail.com>
2019-05-11 09:25:04 +0100

    coding: start to add features to model

---
+++ projects/masd.dogen.coding/src/types/features/type_parameters.cpp
@@ -0,0 +1,86 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include "masd.dogen.coding/types/features/type_parameters.hpp"
+
+namespace masd::dogen::coding::features {
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_type_parameters_variable_number_of_parameters() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("variable_number_of_parameters");
+    r.name().qualified("masd.type_parameters.variable_number_of_parameters");
+    r.value_type(value_type::boolean);
+    r.binding_point(binding_point::element);
+    r.kind(template_kind::instance);
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_type_parameters_count() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("count");
+    r.name().qualified("masd.type_parameters.count");
+    r.value_type(value_type::number);
+    r.binding_point(binding_point::element);
+    r.kind(template_kind::instance);
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_type_parameters_always_in_heap() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("always_in_heap");
+    r.name().qualified("masd.type_parameters.always_in_heap");
+    r.value_type(value_type::boolean);
+    r.binding_point(binding_point::element);
+    r.kind(template_kind::instance);
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+std::list<masd::dogen::variability::meta_model::feature_template>
+type_parameters::make_templates() {
+    using namespace masd::dogen::variability::meta_model;
+    std::list<feature_template> r;
+    r.push_back(make_masd_type_parameters_variable_number_of_parameters());
+    r.push_back(make_masd_type_parameters_count());
+    r.push_back(make_masd_type_parameters_always_in_heap());
+    return r;
+}
+
+}
