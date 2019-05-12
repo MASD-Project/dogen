7352653537b34bedeb913a15c3a85d341b375589
Marco Craveiro <marco.craveiro@gmail.com>
2019-05-12 15:29:51 +0100

    generation.cpp: generate types features

--- projects/masd.dogen.engine/src/types/transforms/context_factory.cpp
+++ projects/masd.dogen.engine/src/types/transforms/context_factory.cpp
@@ -37,6 +37,7 @@
 #include "masd.dogen.generation/types/features/initializer.hpp"
 #include "masd.dogen.generation/types/transforms/model_to_extraction_model_chain.hpp"
 #include "masd.dogen.generation/types/transforms/model_to_extraction_model_transform_registrar.hpp"
+#include "masd.dogen.generation.cpp/types/feature_initializer.hpp"
 #include "masd.dogen.extraction/types/features/initializer.hpp"
 #include "masd.dogen.engine/types/transforms/factory_exception.hpp"
 #include "masd.dogen.engine/types/transforms/context_factory.hpp"
@@ -196,6 +197,7 @@ context context_factory::make_context(const configuration& cfg,
     variability::features::initializer::register_templates(ftrg);
     archetypes::features::initializer::register_templates(ftrg);
     extraction::features::initializer::register_templates(ftrg);
+    generation::cpp::feature_initializer::register_templates(ftrg);
     const auto ftrp(ftrg.repository());
     const auto fm(feature_model_production_chain::apply(vctx, ftrp));
     r.injection_context().feature_model(fm);
