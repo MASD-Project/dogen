005f371b98b4d1c6c2b631b73ad1e36260fdd034
Marco Craveiro <marco.craveiro@gmail.com>
2019-05-12 18:39:21 +0100

    generation.cpp: generate test data's features

--- projects/masd.dogen.generation.cpp/src/types/feature_initializer.cpp
+++ projects/masd.dogen.generation.cpp/src/types/feature_initializer.cpp
@@ -23,6 +23,7 @@
 #include "masd.dogen.generation.cpp/types/formatters/odb/features.hpp"
 #include "masd.dogen.generation.cpp/types/formatters/hash/features.hpp"
 #include "masd.dogen.generation.cpp/types/formatters/types/features.hpp"
+#include "masd.dogen.generation.cpp/types/formatters/test_data/features.hpp"
 
 namespace masd::dogen::generation::cpp {
 
@@ -31,6 +32,7 @@ register_templates(variability::helpers::feature_template_registrar& rg) {
     rg.register_templates(masd::dogen::generation::cpp::formatters::hash::features::make_templates());
     rg.register_templates(masd::dogen::generation::cpp::formatters::io::features::make_templates());
     rg.register_templates(masd::dogen::generation::cpp::formatters::odb::features::make_templates());
+    rg.register_templates(masd::dogen::generation::cpp::formatters::test_data::features::make_templates());
     rg.register_templates(masd::dogen::generation::cpp::formatters::types::features::make_templates());
 
 }
