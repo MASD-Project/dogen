6949fea88b151d9dc7ebd3b94f997a8095d0f1e8
Marco Craveiro <marco.craveiro@gmail.com>
2019-05-13 09:35:39 +0100

    generation.csharp: generate test data's features

---
+++ projects/masd.dogen.generation.csharp/src/types/formatters/test_data/features.cpp
@@ -0,0 +1,163 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include "masd.dogen.variability/types/helpers/value_factory.hpp"
+#include "masd.dogen.generation.csharp/types/formatters/test_data/features.hpp"
+
+namespace masd::dogen::generation::csharp::formatters::test_data {
+
+namespace {
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_directory() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("directory");
+    r.name().qualified("masd.generation.csharp.test_data.directory");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "SequenceGenerators" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_postfix() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("postfix");
+    r.name().qualified("masd.generation.csharp.test_data.postfix");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "SequenceGenerator" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_class_postfix() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("postfix");
+    r.name().qualified("masd.generation.csharp.test_data.class.postfix");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_assistant_postfix() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("postfix");
+    r.name().qualified("masd.generation.csharp.test_data.assistant.postfix");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_enum_postfix() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("postfix");
+    r.name().qualified("masd.generation.csharp.test_data.enum.postfix");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+masd::dogen::variability::meta_model::feature_template
+make_masd_generation_csharp_test_data_primitive_postfix() {
+    using namespace masd::dogen::variability::meta_model;
+    feature_template r;
+    r.name().simple("postfix");
+    r.name().qualified("masd.generation.csharp.test_data.primitive.postfix");
+    const auto vt(value_type::text);
+    r.value_type(vt);
+    r.binding_point(binding_point::global);
+    r.kind(template_kind::instance);
+    masd::dogen::variability::helpers::value_factory f;
+    r.default_value(f.make(vt, std::list<std::string>{ "" }));
+
+    archetypes::location al;
+    al.kernel("masd");
+
+     r.location(al);
+     return r;
+}
+
+}
+
+std::list<masd::dogen::variability::meta_model::feature_template>
+features::make_templates() {
+    using namespace masd::dogen::variability::meta_model;
+    std::list<feature_template> r;
+    r.push_back(make_masd_generation_csharp_test_data_directory());
+    r.push_back(make_masd_generation_csharp_test_data_postfix());
+    r.push_back(make_masd_generation_csharp_test_data_class_postfix());
+    r.push_back(make_masd_generation_csharp_test_data_assistant_postfix());
+    r.push_back(make_masd_generation_csharp_test_data_enum_postfix());
+    r.push_back(make_masd_generation_csharp_test_data_primitive_postfix());
+    return r;
+}
+
+}
