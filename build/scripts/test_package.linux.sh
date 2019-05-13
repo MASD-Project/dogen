427ca9a4babeb5402bb43274aa51075d96e23af5
Marco Craveiro <marco.craveiro@gmail.com>
2019-05-13 16:28:48 +0100

    scripts: fix path to hello world model

--- build/scripts/test_package.linux.sh
+++ build/scripts/test_package.linux.sh
@@ -63,7 +63,7 @@ build_type_dir="${compiler_dir}/${build_type}";
 # Test the package
 #
 sudo dpkg -i ${build_type_dir}/stage/pkg/dogen_*_amd64-applications.deb
-cp ${product_dir}/projects/masd.dogen.models/dia/hello_world.dia /tmp
+cp ${product_dir}/projects/dogen.models/dia/hello_world.dia /tmp
 cd /tmp
 /usr/bin/dogen.cli --version
 /usr/bin/dogen.cli generate -t hello_world.dia
