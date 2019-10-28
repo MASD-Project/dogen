db1f96f19bb563aaa71783297e96363a32a21a44
Marco Craveiro <marco.craveiro@gmail.com>
2019-10-28 08:48:36 +0000

    generation: remove checks for odb library
    This check was actually not very useful because we do not know how the
    user has detected CMake. By adding this check we widen the interface
    between the dogen generated CMake and the manually crafted CMake for
    no good reason. Instead, it is the responsibility of the calling code
    to ensure that ODB was detected and configured correctly.

--- projects/dogen.generation.cpp/src/types/formatters/source_cmakelists_formatter.cpp
+++ projects/dogen.generation.cpp/src/types/formatters/source_cmakelists_formatter.cpp
@@ -104,10 +104,6 @@ a.stream() << "    \"${CMAKE_CURRENT_SOURCE_DIR}/\"" << std::endl;
 a.stream() << "    \"${CMAKE_CURRENT_SOURCE_DIR}/*." << c.implementation_file_extension() << "\")" << std::endl;
 a.stream() << std::endl;
         if (a.is_odb_facet_enabled() && !c.odb_targets().targets().empty()) {
-a.stream() << "if(NOT ODB_LIBODB_FOUND)" << std::endl;
-a.stream() << "    message(FATAL_ERROR \"ODB include directories not defined.\")" << std::endl;
-a.stream() << "endif()" << std::endl;
-a.stream() << std::endl;
 a.stream() << "set(odb_files \"\")" << std::endl;
 a.stream() << "file(GLOB_RECURSE odb_files RELATIVE" << std::endl;
 a.stream() << "   \"${CMAKE_CURRENT_SOURCE_DIR}/\"" << std::endl;
