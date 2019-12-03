f327b957beaf9d1d7587390f36c8f5043d6e3bff
Marco Craveiro <marco.craveiro@gmail.com>
2019-12-03 00:03:43 +0000

    generation.cpp: remove odb pragma processing for primitives and objects

--- projects/dogen.generation.cpp/src/types/formattables/odb_expander.cpp
+++ projects/dogen.generation.cpp/src/types/formattables/odb_expander.cpp
@@ -24,8 +24,6 @@
 #include "dogen.utility/types/io/unordered_map_io.hpp"
 #include "dogen.variability/types/helpers/feature_selector.hpp"
 #include "dogen.variability/types/helpers/configuration_selector.hpp"
-#include "dogen.assets/types/meta_model/structural/object.hpp"
-#include "dogen.assets/types/meta_model/structural/primitive.hpp"
 #include "dogen.generation.cpp/types/formattables/adapter.hpp"
 #include "dogen.generation.cpp/types/fabric/common_odb_options.hpp"
 #include "dogen.generation.cpp/types/fabric/object_odb_options.hpp"
@@ -56,70 +54,20 @@ namespace dogen::generation::cpp::formattables {
 
 class updator : public element_visitor {
 public:
-    updator(model& fm, const locator& l,
-        const odb_expander::feature_group& fg);
-
-private:
-    std::string make_type_overrides(const std::unordered_map<
-        assets::meta_model::orm::database_system, std::string>& type_overrides)
-    const;
-
-    std::list<std::string> make_odb_pragmas(
-        const variability::meta_model::configuration& cfg) const;
+    updator(model& fm, const locator& l);
 
 public:
     using element_visitor::visit;
     void visit(fabric::common_odb_options& coo);
     void visit(fabric::object_odb_options& ooo);
-    void visit(assets::meta_model::structural::object& o);
-    void visit(assets::meta_model::structural::primitive& p);
-
-public:
-    const boost::optional<odb_properties>& result() const;
 
 private:
     const model& model_;
     const locator locator_;
-    const odb_expander::feature_group& feature_group_;
-    boost::optional<odb_properties> result_;
 };
 
-updator::
-updator(model& fm, const locator& l, const odb_expander::feature_group& fg)
-    : model_(fm), locator_(l), feature_group_(fg) {}
-
-std::string
-updator::make_type_overrides(const std::unordered_map<
-    assets::meta_model::orm::database_system, std::string>& type_overrides)
-    const {
-    if (!type_overrides.empty())
-        return std::string();
-
-    std::ostringstream s;
-    bool is_first(true);
-    for (const auto pair : type_overrides) {
-        if (!is_first)
-            s << " ";
-
-        const auto ds(pair.first);
-        const auto type(pair.second);
-        s << adapter::to_odb_database(ds)
-          << ":type(\"" << type << "\")";
-        is_first = false;
-    }
-
-    return s.str();
-}
-
-std::list<std::string> updator::make_odb_pragmas(
-    const variability::meta_model::configuration& cfg) const {
-
-    const variability::helpers::configuration_selector s(cfg);
-    if (!s.has_configuration_point(feature_group_.odb_pragma))
-        return std::list<std::string>();
-
-    return s.get_text_collection_content(feature_group_.odb_pragma);
-}
+updator::updator(model& fm, const locator& l)
+    : model_(fm), locator_(l) {}
 
 void updator::visit(fabric::common_odb_options& coo) {
     coo.databases(model_.odb_databases());
@@ -155,144 +103,6 @@ void updator::visit(fabric::object_odb_options& ooo) {
     ooo.header_guard_prefix(header_guard_factory::make(odb_rp.parent_path()));
 }
 
-void updator::visit(assets::meta_model::structural::object& o) {
-    odb_properties op;
-
-    auto top_level_pragmas(make_odb_pragmas(*o.configuration()));
-    if (o.orm_properties()) {
-        /*
-         * If the user has supplied an ORM properties and this
-         * object does not have a primary key, we need to inject ODB's
-         * pragma for it.
-         */
-        const auto& cfg(*o.orm_properties());
-        op.is_value(cfg.is_value());
-
-        if (cfg.generate_mapping() && !op.is_value() && !cfg.has_primary_key())
-            top_level_pragmas.push_back(no_id_pragma);
-
-        using assets::meta_model::orm::letter_case;
-        const auto& sn(cfg.schema_name());
-        if (!sn.empty() && (op.is_value() || cfg.generate_mapping())) {
-            std::ostringstream s;
-            s << "schema(\"" << cfg.capitalised_schema_name() << "\")";
-            top_level_pragmas.push_back(s.str());
-        }
-    }
-
-    op.top_level_odb_pragmas(top_level_pragmas);
-
-    for (const auto& attr : o.local_attributes()) {
-        const auto id(attr.name().qualified().dot());
-        auto attr_pragmas(make_odb_pragmas(*attr.configuration()));
-
-        if (attr.orm_properties()) {
-            const auto& cfg(*attr.orm_properties());
-            if (cfg.is_primary_key())
-                attr_pragmas.push_back(id_pragma);
-
-            /*
-             * For composite keys, we do not want to use the column
-             * name as a prefix.
-             */
-            if (cfg.is_composite())
-                attr_pragmas.push_back(empty_column_attribute);
-
-            if (cfg.is_nullable()) {
-                if (*cfg.is_nullable())
-                    attr_pragmas.push_back(null_pragma);
-                else
-                    attr_pragmas.push_back(not_null_pragma);
-            }
-
-            if (!cfg.type_overrides().empty()) {
-                const auto to(make_type_overrides(cfg.type_overrides()));
-                attr_pragmas.push_back(to);
-            }
-        }
-
-        if (attr_pragmas.empty())
-            continue;
-
-        op.attribute_level_odb_pragmas()[id] = attr_pragmas;
-    }
-
-    const bool has_top_level_pragmas(!op.top_level_odb_pragmas().empty());
-    const bool has_attribute_pragmas(!op.attribute_level_odb_pragmas().empty());
-    const bool has_pragmas(has_top_level_pragmas || has_attribute_pragmas);
-
-    if (has_pragmas)
-        result_ = op;
-}
-
-void updator::visit(assets::meta_model::structural::primitive& p) {
-    odb_properties op;
-    op.is_value(true);
-    op.top_level_odb_pragmas(make_odb_pragmas(*p.configuration()));
-
-    if (p.orm_properties()) {
-        const auto& cfg(*p.orm_properties());
-
-        const auto& sn(cfg.schema_name());
-        if (!sn.empty() && cfg.generate_mapping()) {
-            std::ostringstream s;
-            s << "schema(\"" << cfg.capitalised_schema_name() << "\")";
-            op.top_level_odb_pragmas().push_back(s.str());
-        }
-
-        for (const auto& tm: cfg.type_mappings()) {
-            std::ostringstream s;
-            s << "map ";
-            if (tm.database())
-                s << adapter::to_odb_database(*tm.database()) << ":";
-
-            s << "type(\"" << tm.source_type() << "\") as (\""
-              << tm.destination_type() << "\")";
-
-            if (!tm.to_source_type().empty())
-                s << "to(\"" << tm.to_source_type() << "\")";
-
-            if (!tm.to_destination_type().empty())
-                s << "to(\"" << tm.to_destination_type() << "\")";
-
-            op.top_level_odb_pragmas().push_back(s.str());
-        }
-    }
-
-    const bool has_top_level_pragmas(!op.top_level_odb_pragmas().empty());
-    if (!has_top_level_pragmas)
-        return;
-
-    /*
-     * For primitives, we need to inject manually the attribute level
-     * pragmas, if there was a top-level pragma. This is because users
-     * cannot see the internal attribute.
-     *
-     * We add this pragma so as to name the primitive's internal value
-     * attribute correctly. Primitives do not have column names; they
-     * are set by the type that owns them. This avoids names such as
-     * "primitive_id_primitive_id", which become "primitive_id"
-     * instead.
-     *
-     * In addition, we also need to obtain all of the type overrides.
-     */
-    std::ostringstream s;
-    s << empty_column_attribute;
-    if (p.orm_properties() && !p.orm_properties()->type_overrides().empty())
-        s << make_type_overrides(p.orm_properties()->type_overrides());
-    const auto pragmas = std::list<std::string> { s.str() };
-
-    const auto& attr(p.value_attribute());
-    const auto id(attr.name().qualified().dot());
-    op.attribute_level_odb_pragmas()[id] = pragmas;
-    result_ = op;
-}
-
-const boost::optional<odb_properties>&
-updator::result() const {
-    return result_;
-}
-
 odb_expander::feature_group odb_expander::
 make_feature_group(const variability::meta_model::feature_model& fm) const {
     BOOST_LOG_SEV(lg, debug) << "Creating feature groups.";
@@ -307,12 +117,11 @@ make_feature_group(const variability::meta_model::feature_model& fm) const {
 }
 
 void odb_expander::expand(
-    const variability::meta_model::feature_model& feature_model,
+    const variability::meta_model::feature_model& /*feature_model*/,
     const locator& l, model& fm) const {
 
     BOOST_LOG_SEV(lg, debug) << "Started expanding odb properties.";
 
-    const auto fg(make_feature_group(feature_model));
     for (auto& pair : fm.formattables()) {
         const auto id(pair.first);
         BOOST_LOG_SEV(lg, debug) << "Procesing element: " << id;
@@ -331,14 +140,8 @@ void odb_expander::expand(
 
         for (const auto& ptr : formattable.all_segments()) {
             auto& e(*ptr);
-            updator g(fm, l, fg);
+            updator g(fm, l);
             e.accept(g);
-
-            if (!g.result())
-                continue;
-
-            auto& eprops(formattable.element_properties());
-            eprops.odb_properties(*g.result());
         }
     }
 
