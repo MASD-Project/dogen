0d609d0ed6bd7d59627bf8ef88045b3525502340
Marco Craveiro <marco.craveiro@gmail.com>
2019-12-14 21:03:42 +0000

    relational: lots of new queries

--- projects/dogen.relational/sql/cheat_sheet.sql
+++ projects/dogen.relational/sql/cheat_sheet.sql
@@ -105,6 +105,35 @@ select "TIMESTAMP", "RUN_ID", "VERSION", "ACTIVITY", "LOGGING_IMPACT", "TRACING_
 --
 select "TIMESTAMP", "RUN_ID", "ACTIVITY" from "RUN_EVENT" where "EVENT_TYPE" = 1 order by "TIMESTAMP";
 
+--
+-- All transforms in a run
+--
+select "TIMESTAMP", "TRANSFORM_ID", (CASE WHEN "TRANSFORM_TYPE" = 1 THEN  'CHAIN' ELSE 'LEAF' END) "TRANSFORM_TYPE",
+(CASE WHEN "EVENT_TYPE" = 1 THEN  'START' ELSE 'END' END), "PARENT_TRANSFORM", "TRANSFORM_INSTANCE_ID",
+cast("PAYLOAD" as varchar(20)), "PAYLOAD"->'__type__' "PAYLOAD_TYPE"
+from "TRANSFORM_EVENT"
+where "RUN_ID" = 'd92263a0-4965-4a3e-803d-1e2851e96fcc'
+order by "TIMESTAMP" asc;
+
+--
+-- Top-level transforms in a run
+--
+select "TIMESTAMP", "TRANSFORM_ID", (CASE WHEN "TRANSFORM_TYPE" = 1 THEN  'CHAIN' ELSE 'LEAF' END) "TRANSFORM_TYPE",
+(CASE WHEN "EVENT_TYPE" = 1 THEN  'START' ELSE 'END' END), "PARENT_TRANSFORM", "TRANSFORM_INSTANCE_ID",
+cast("PAYLOAD" as varchar(20)), "PAYLOAD"->'__type__' "PAYLOAD_TYPE"
+from "TRANSFORM_EVENT"
+where "RUN_ID" = 'd92263a0-4965-4a3e-803d-1e2851e96fcc' and coalesce("PARENT_TRANSFORM", '') = ''
+order by "TIMESTAMP" asc;
+
+--
+-- Transforms by parents
+--
+select "TIMESTAMP", "TRANSFORM_ID", (CASE WHEN "TRANSFORM_TYPE" = 1 THEN  'CHAIN' ELSE 'LEAF' END) "TRANSFORM_TYPE",
+(CASE WHEN "EVENT_TYPE" = 1 THEN  'START' ELSE 'END' END), "PARENT_TRANSFORM", "TRANSFORM_INSTANCE_ID",
+cast("PAYLOAD" as varchar(20)), "PAYLOAD"->'__type__' "PAYLOAD_TYPE"
+from "TRANSFORM_EVENT"
+where "PARENT_TRANSFORM" = 'cb25de94-fe06-4ec0-bb8d-eb7928066221'
+order by "TIMESTAMP" asc;
 
 --
 -- Navigating into transforms
@@ -143,3 +172,12 @@ from (
 -- Get logging
 --
 select * from "LOG_EVENT" limit 30;
+
+--
+-- Log events for an interval
+--
+select "TIMESTAMP", "COMPONENT",  "SEVERITY", cast("MESSAGE" as varchar(100))
+from "LOG_EVENT"
+where "TIMESTAMP" between ' 2019-12-14 20:44:07.77754' and '2019-12-14 20:45:11.619425'
+and "RUN_ID" = 'd92263a0-4965-4a3e-803d-1e2851e96fcc'
+order by "TIMESTAMP";
