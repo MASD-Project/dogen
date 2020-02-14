52f04bc77d32ee7f384b19500d459b7824853e23
Marco Craveiro <marco.craveiro@gmail.com>
2020-02-14 10:36:41 +0000

    assets: add msbuild targets to meta-name factory

--- projects/dogen.assets/src/types/helpers/meta_name_factory.cpp
+++ projects/dogen.assets/src/types/helpers/meta_name_factory.cpp
@@ -152,6 +152,12 @@ meta_model::name meta_name_factory::make_visual_studio_project_name() {
     return r;
 }
 
+meta_model::name meta_name_factory::make_visual_studio_msbuild_targets() {
+    static const auto r(
+        make_name("build_visual_studio_msbuild_targets"));
+    return r;
+}
+
 meta_model::name meta_name_factory::make_orm_common_odb_options_name() {
     static const auto r(
         make_name("orm_common_odb_options"));
