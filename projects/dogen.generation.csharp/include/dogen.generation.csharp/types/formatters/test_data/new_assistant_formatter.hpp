242431ed8397dcbcecc944ce0a773842f7b1e1dc
Marco Craveiro <marco.craveiro@gmail.com>
2020-02-15 22:59:32 +0000

    generation.csharp: add new formatters for assistant

---
+++ projects/dogen.generation.csharp/include/dogen.generation.csharp/types/formatters/test_data/new_assistant_formatter.hpp
@@ -0,0 +1,57 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_GENERATION_CSHARP_TYPES_FORMATTERS_TEST_DATA_NEW_ASSISTANT_FORMATTER_HPP
+#define DOGEN_GENERATION_CSHARP_TYPES_FORMATTERS_TEST_DATA_NEW_ASSISTANT_FORMATTER_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <algorithm>
+#include "dogen.generation.csharp/types/formatters/artefact_formatter_interface.hpp"
+
+namespace dogen::generation::csharp::formatters::test_data {
+
+class new_assistant_formatter final : public artefact_formatter_interface {
+public:
+    static std::string static_id();
+
+public:
+    std::string id() const override;
+    archetypes::location archetype_location() const override;
+    const assets::meta_model::name& meta_name() const override;
+    std::string family() const override;
+
+public:
+    std::list<std::string> inclusion_dependencies(
+        const assets::meta_model::element& e) const override;
+
+    boost::filesystem::path full_path(
+        const formattables::locator& l, const assets::meta_model::name& n) const override;
+
+public:
+    extraction::meta_model::artefact format(const context& ctx,
+        const assets::meta_model::element& e) const override;
+};
+
+}
+
+#endif
