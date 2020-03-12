71e12a28084f2cb7a152ada7d45c155af243f229
Marco Craveiro <marco.craveiro@gmail.com>
2020-03-12 09:31:20 +0000

    archetypes: generate skeleton for template instantiation domains factory

---
+++ projects/dogen.archetypes/include/dogen.archetypes/types/template_instantiation_domains_factory.hpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_ARCHETYPES_TYPES_TEMPLATE_INSTANTIATION_DOMAINS_FACTORY_HPP
+#define DOGEN_ARCHETYPES_TYPES_TEMPLATE_INSTANTIATION_DOMAINS_FACTORY_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <algorithm>
+
+namespace dogen::archetypes {
+
+class template_instantiation_domains_factory final {
+public:
+    template_instantiation_domains_factory() = default;
+    template_instantiation_domains_factory(const template_instantiation_domains_factory&) = default;
+    template_instantiation_domains_factory(template_instantiation_domains_factory&&) = default;
+    ~template_instantiation_domains_factory() = default;
+    template_instantiation_domains_factory& operator=(const template_instantiation_domains_factory&) = default;
+
+public:
+    bool operator==(const template_instantiation_domains_factory& rhs) const;
+    bool operator!=(const template_instantiation_domains_factory& rhs) const {
+        return !this->operator==(rhs);
+    }
+
+};
+
+}
+
+#endif
