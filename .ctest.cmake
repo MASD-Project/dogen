5671feb893682f425774864233d928f3ff3e9f53
Marco Craveiro <marco.craveiro@gmail.com>
2020-04-26 08:28:23 +0100

    ctest: do not use os version in build names
    Every time we upgrade the kernel the nightly build changes name and we
    can't see the historic build times properly due to this.

--- .ctest.cmake
+++ .ctest.cmake
@@ -112,7 +112,7 @@ set(SITE "${CTEST_SITE}")
 set(CTEST_CMAKE_GENERATOR "${generator}")
 set(CTEST_CONFIGURATION_TYPE "${configuration_type}")
 set(CTEST_BUILD_NAME
-    "${compiler}-${CMAKE_SYSTEM}-${CMAKE_SYSTEM_PROCESSOR}-${configuration_type}")
+    "${compiler}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-${configuration_type}")
 set(CTEST_BUILD_TARGET "package")
 set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
 set(build_folder "${compiler}/${CTEST_CONFIGURATION_TYPE}")
