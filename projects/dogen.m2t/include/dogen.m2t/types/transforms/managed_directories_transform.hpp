4b9faa60eaff7fc1a0f6039210e022bb47ede5ee
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-01 14:22:56 +0100

    m2t: generate managed directories transform

---
+++ projects/dogen.m2t/include/dogen.m2t/types/transforms/managed_directories_transform.hpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_M2T_TYPES_TRANSFORMS_MANAGED_DIRECTORIES_TRANSFORM_HPP
+#define DOGEN_M2T_TYPES_TRANSFORMS_MANAGED_DIRECTORIES_TRANSFORM_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <algorithm>
+
+namespace dogen::m2t::transforms {
+
+class managed_directories_transform final {
+public:
+    managed_directories_transform() = default;
+    managed_directories_transform(const managed_directories_transform&) = default;
+    managed_directories_transform(managed_directories_transform&&) = default;
+    ~managed_directories_transform() = default;
+    managed_directories_transform& operator=(const managed_directories_transform&) = default;
+
+public:
+    bool operator==(const managed_directories_transform& rhs) const;
+    bool operator!=(const managed_directories_transform& rhs) const {
+        return !this->operator==(rhs);
+    }
+
+};
+
+}
+
+#endif
