fcc64a1f3f37739acae78e476f42076d305bc738
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-09 16:15:29 +0100

    text: commit current state of text to text transform

--- projects/dogen.text/src/types/transforms/text_to_text_chain.cpp
+++ projects/dogen.text/src/types/transforms/text_to_text_chain.cpp
@@ -18,8 +18,12 @@
  * MA 02110-1301, USA.
  *
  */
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
 #include "dogen.utility/types/log/logger.hpp"
 #include "dogen.tracing/types/scoped_tracer.hpp"
+#include "dogen.physical/types/entities/meta_model.hpp"
 #include "dogen.text/io/entities/model_io.hpp"
 #include "dogen.text/types/transforms/transformation_error.hpp"
 #include "dogen.text/types/transforms/text_to_text_chain.hpp"
@@ -34,6 +38,8 @@ const std::string unsupported_technical_space(
     "Could not find transform for technical space: ");
 const std::string disabled_transform(
     "Transform for requested technical space is disabled: ");
+const std::string no_logical_meta_id(
+    "Logical meta-model ID empty for archetype: ");
 
 }
 
@@ -54,6 +60,47 @@ void text_to_text_chain::apply(const text::transforms::context& ctx,
     text::entities::model& m) {
     tracing::scoped_chain_tracer stp(lg, "text to text chain", transform_id,
         m.name().qualified().dot(), *ctx.tracer(), m);
+
+    /*
+     * First we must find all archetypes which required text to text
+     * transforms.
+     */
+    std::unordered_map<std::string, std::list<physical::entities::archetype>>
+        archetypes_for_text_to_text_transform;
+    std::unordered_set<std::string> logical_meta_elements_of_interest;
+    const auto& pmm(*ctx.physical_meta_model());
+    for (const auto& kernel_pair : pmm.kernels()) {
+        for (const auto& backend_pair : kernel_pair.second.backends()) {
+            for (const auto& archetype_pair : backend_pair.archetypes()) {
+                const auto& arch(archetype_pair.second);
+                const auto t2tt(arch.text_to_text_transform());
+                const auto lmeid(arch.logical_meta_element_id());
+                if (!t2tt.empty()) {
+                    if (lmeid.empty()) {
+                        const auto mn(arch.meta_name().qualified());
+                        BOOST_LOG_SEV(lg, error) << no_logical_meta_id << mn;
+                        BOOST_THROW_EXCEPTION(
+                            transformation_error(no_logical_meta_id + mn));
+                    }
+                    logical_meta_elements_of_interest.insert(lmeid);
+                    archetypes_for_text_to_text_transform[t2tt].push_back(arch);
+                }
+            }
+        }
+    }
+
+    /*
+     * If there are none, our job is done.
+     */
+    if (archetypes_for_text_to_text_transform.empty()) {
+        BOOST_LOG_SEV(lg, warn) << "No archetypes need T2T transforms.";
+        return;
+    }
+
+    /*
+     *
+     */
+
 
     stp.end_chain(m);
 }
