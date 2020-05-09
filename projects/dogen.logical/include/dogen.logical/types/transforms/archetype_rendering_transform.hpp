c8d95191e43adfa34d1d9b355f4b4ffd53358a87
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-09 16:18:45 +0100

    logical: generate archetype rendering transform

---
+++ projects/dogen.logical/include/dogen.logical/types/transforms/archetype_rendering_transform.hpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_LOGICAL_TYPES_TRANSFORMS_ARCHETYPE_RENDERING_TRANSFORM_HPP
+#define DOGEN_LOGICAL_TYPES_TRANSFORMS_ARCHETYPE_RENDERING_TRANSFORM_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <algorithm>
+
+namespace dogen::logical::transforms {
+
+class archetype_rendering_transform final {
+public:
+    archetype_rendering_transform() = default;
+    archetype_rendering_transform(const archetype_rendering_transform&) = default;
+    archetype_rendering_transform(archetype_rendering_transform&&) = default;
+    ~archetype_rendering_transform() = default;
+    archetype_rendering_transform& operator=(const archetype_rendering_transform&) = default;
+
+public:
+    bool operator==(const archetype_rendering_transform& rhs) const;
+    bool operator!=(const archetype_rendering_transform& rhs) const {
+        return !this->operator==(rhs);
+    }
+
+};
+
+}
+
+#endif
