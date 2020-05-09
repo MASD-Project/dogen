1ff8a8794bbdd575fbdfb69335bedec173bf5428
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-09 11:03:42 +0100

    text: add wale features

---
+++ projects/dogen.text/src/types/features/wale.cpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include "dogen.text/types/features/wale.hpp"
+#include "dogen.variability/types/helpers/value_factory.hpp"
+#include "dogen.variability/types/helpers/feature_selector.hpp"
+#include "dogen.variability/types/helpers/configuration_selector.hpp"
+
+namespace dogen::text::features {
+wale::feature_group
+wale::make_feature_group(const dogen::variability::entities::feature_model& fm) {
+    feature_group r;
+    const dogen::variability::helpers::feature_selector s(fm);
+
+    r.text_template = s.get_by_name("masd.stitch.wale.text_template");
+    r.kvp = s.get_by_name("masd.stitch.wale.kvp");
+
+    return r;
+}
+
+wale::static_configuration wale::make_static_configuration(
+    const feature_group& fg,
+    const dogen::variability::entities::configuration& cfg) {
+
+    static_configuration r;
+    const dogen::variability::helpers::configuration_selector s(cfg);
+    if (s.has_configuration_point(fg.text_template))
+        r.text_template = s.get_text_content(fg.text_template);
+    if (s.has_configuration_point(fg.kvp))
+        r.kvp = s.get_key_value_pair_content(fg.kvp);
+    return r;
+}
+}
