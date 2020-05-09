2b3420998aca7b9d4c7d787b338286e31519a221
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-09 17:42:11 +0100

    logical: generate decoration related classes

---
+++ projects/dogen.logical/include/dogen.logical/types/helpers/decoration_configuration_factory.hpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef DOGEN_LOGICAL_TYPES_HELPERS_DECORATION_CONFIGURATION_FACTORY_HPP
+#define DOGEN_LOGICAL_TYPES_HELPERS_DECORATION_CONFIGURATION_FACTORY_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <algorithm>
+
+namespace dogen::logical::helpers {
+
+class decoration_configuration_factory final {
+public:
+    decoration_configuration_factory() = default;
+    decoration_configuration_factory(const decoration_configuration_factory&) = default;
+    decoration_configuration_factory(decoration_configuration_factory&&) = default;
+    ~decoration_configuration_factory() = default;
+    decoration_configuration_factory& operator=(const decoration_configuration_factory&) = default;
+
+public:
+    bool operator==(const decoration_configuration_factory& rhs) const;
+    bool operator!=(const decoration_configuration_factory& rhs) const {
+        return !this->operator==(rhs);
+    }
+
+};
+
+}
+
+#endif
