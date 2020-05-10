d8a8bfc1e2e0a7348409543ff7383c631dbb323e
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-10 17:45:14 +0100

    text.cpp: generate new archetype formatter

--- projects/dogen.text.cpp/src/types/transforms/types/archetype_class_implementation_transform_new.cpp
+++ projects/dogen.text.cpp/src/types/transforms/types/archetype_class_implementation_transform_new.cpp
@@ -1,2 +1,87 @@
-// dummy function to suppress ranlib warnings
-void archetype_class_implementation_transform_new() { }
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include <boost/throw_exception.hpp>
+#include "dogen.text.cpp/types/transforms/formatting_error.hpp"
+#include "dogen.utility/types/log/logger.hpp"
+#include "dogen.physical/types/helpers/meta_name_factory.hpp"
+#include "dogen.logical/types/entities/physical/archetype.hpp"
+#include "dogen.logical/types/helpers/meta_name_factory.hpp"
+#include "dogen.text.cpp/types/transforms/assistant.hpp"
+#include "dogen.text.cpp/types/transforms/types/archetype_class_implementation_transform_new.hpp"
+#include "dogen.text.cpp/types/traits.hpp"
+#include "dogen.text.cpp/types/transforms/types/traits.hpp"
+
+namespace dogen::text::cpp::transforms::types {
+physical::entities::archetype archetype_class_implementation_transform_new::static_archetype() const {
+    static physical::entities::archetype r([]() {
+        physical::entities::archetype r;
+        using pmnf = physical::helpers::meta_name_factory;
+        r.meta_name(pmnf::make(cpp::traits::backend_sn(),
+            traits::facet_sn(), traits::archetype_class_implementation_archetype_sn()));
+        using lmnf = logical::helpers::meta_name_factory;
+        r.logical_meta_element_id(lmnf::make_physical_archetype_name().qualified().dot());
+        r.text_to_text_transform("dogen.text.transforms.stitch_transform");
+        return r;
+    }());
+    return r;
+}
+
+physical::entities::archetype archetype_class_implementation_transform_new::archetype() const {
+    return static_archetype();
+}
+
+inclusion_support_types archetype_class_implementation_transform_new::inclusion_support_type() const {
+    return inclusion_support_types::not_supported;
+}
+
+boost::filesystem::path archetype_class_implementation_transform_new::inclusion_path(
+    const formattables::locator& /*l*/, const logical::entities::name& n) const {
+
+    using namespace dogen::utility::log;
+    static logger lg(logger_factory(archetype().meta_name().qualified()));
+    static const std::string not_supported("Inclusion path is not supported: ");
+
+    BOOST_LOG_SEV(lg, error) << not_supported << n.qualified().dot();
+    BOOST_THROW_EXCEPTION(formatting_error(not_supported + n.qualified().dot()));
+}
+
+boost::filesystem::path archetype_class_implementation_transform_new::full_path(
+    const formattables::locator& l, const logical::entities::name& n) const {
+    return l.make_full_path_for_cpp_implementation(n, archetype().meta_name().qualified());
+}
+
+std::list<std::string> archetype_class_implementation_transform_new::inclusion_dependencies(
+    const formattables::dependencies_builder_factory& /*f*/,
+    const logical::entities::element& /*e*/) const {
+    static std::list<std::string> r;
+    return r;
+}
+
+void archetype_class_implementation_transform_new::apply(const context& ctx, const logical::entities::element& e,
+    physical::entities::artefact& a) const {
+    assistant ast(ctx, e, archetype().meta_name(), false/*requires_header_guard*/, a);
+    const auto& arch(ast.as<logical::entities::physical::archetype>(e));
+    a.overwrite(true);
+    ast.update_artefact();
+    a.content(arch.rendered_stitch_template());
+}
+
+}
