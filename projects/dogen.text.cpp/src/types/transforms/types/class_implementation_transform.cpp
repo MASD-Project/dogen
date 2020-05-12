e2b91255898c56fcf2c66c10f4d237fa48d520d5
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-12 22:06:46 +0100

    text.cpp: promote class implementation to artefact

--- projects/dogen.text.cpp/src/types/transforms/types/class_implementation_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/types/class_implementation_transform.cpp
@@ -35,7 +35,6 @@
 #include "dogen.text.cpp/types/transforms/types/class_implementation_transform.hpp"
 
 namespace dogen::text::cpp::transforms::types {
-
 physical::entities::archetype class_implementation_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
