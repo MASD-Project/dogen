39c3783fdd692301c03b0a94e1ed8a8e931d1b36
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-13 22:16:13 +0100

    text.cpp: promote builtin header to artefact

--- projects/dogen.text.cpp/src/types/transforms/types/builtin_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/types/builtin_header_transform.cpp
@@ -29,7 +29,6 @@
 #include "dogen.text.cpp/types/transforms/types/builtin_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::types {
-
 physical::entities::archetype builtin_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
