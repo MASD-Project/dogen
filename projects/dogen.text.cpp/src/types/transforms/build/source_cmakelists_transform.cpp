1a1493e7f3feab125a32aba07f3f958596d3ac82
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-15 21:46:35 +0100

    text.cpp: promote source cmakelists in build to archetype

--- projects/dogen.text.cpp/src/types/transforms/build/source_cmakelists_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/build/source_cmakelists_transform.cpp
@@ -34,7 +34,6 @@
 #include "dogen.text.cpp/types/transforms/build/source_cmakelists_transform.hpp"
 
 namespace dogen::text::cpp::transforms::build {
-
 physical::entities::archetype source_cmakelists_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
@@ -52,8 +51,7 @@ physical::entities::archetype source_cmakelists_transform::archetype() const {
     return static_archetype();
 }
 
-inclusion_support_types source_cmakelists_transform::
-inclusion_support_type() const {
+inclusion_support_types source_cmakelists_transform::inclusion_support_type() const {
     return inclusion_support_types::not_supported;
 }
 
@@ -61,9 +59,7 @@ boost::filesystem::path source_cmakelists_transform::inclusion_path(
     const formattables::locator& /*l*/, const logical::entities::name& n) const {
 
     using namespace dogen::utility::log;
-    using namespace dogen::text::cpp::transforms;
     static logger lg(logger_factory(archetype().meta_name().qualified()));
-
     static const std::string not_supported("Inclusion path is not supported: ");
 
     BOOST_LOG_SEV(lg, error) << not_supported << n.qualified().dot();
