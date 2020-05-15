78508967839ecc6defd6147ba770b86d9d9e1880
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-15 17:38:24 +0100

    text.cpp: promote class header in hash to archetype

--- projects/dogen.text.cpp/src/types/transforms/hash/class_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/hash/class_header_transform.cpp
@@ -34,7 +34,6 @@
 #include "dogen.text.cpp/types/transforms/hash/class_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::hash {
-
 physical::entities::archetype class_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
