8f3f3ba57a65ef63adc907c0482348b61c4c972e
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-15 19:02:44 +0100

    text.cpp: promote class implementation to archetype

--- projects/dogen.text.cpp/src/types/transforms/hash/class_implementation_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/hash/class_implementation_transform.cpp
@@ -34,7 +34,6 @@
 #include "dogen.text.cpp/types/transforms/hash/class_implementation_transform.hpp"
 
 namespace dogen::text::cpp::transforms::hash {
-
 physical::entities::archetype class_implementation_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
