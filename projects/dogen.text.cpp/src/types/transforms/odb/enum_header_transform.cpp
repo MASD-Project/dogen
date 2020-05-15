55f95b82b39c6c50aeed6014249748416dc6a3fa
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-15 17:17:15 +0100

    text.cpp: promote primitive and enum header in odb to archetype

--- projects/dogen.text.cpp/src/types/transforms/odb/enum_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/odb/enum_header_transform.cpp
@@ -31,7 +31,6 @@
 #include "dogen.text.cpp/types/transforms/odb/enum_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::odb {
-
 physical::entities::archetype enum_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
--- projects/dogen.text.cpp/src/types/transforms/odb/primitive_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/odb/primitive_header_transform.cpp
@@ -30,7 +30,6 @@
 #include "dogen.text.cpp/types/transforms/odb/primitive_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::odb {
-
 physical::entities::archetype primitive_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
