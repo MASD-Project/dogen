3f784ce4027ecbe77250ec4f1c7684158085b968
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-16 18:13:39 +0100

    text.cpp: promote builtin implementation in io to archetype

--- projects/dogen.text.cpp/src/types/transforms/io/builtin_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/io/builtin_header_transform.cpp
@@ -29,7 +29,6 @@
 #include "dogen.text.cpp/types/transforms/io/builtin_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::io {
-
 physical::entities::archetype builtin_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
