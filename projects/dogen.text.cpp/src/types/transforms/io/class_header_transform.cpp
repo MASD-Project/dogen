8965d0f509cca5795fb41ce8799a8bb415cf6231
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-16 17:39:19 +0100

    text.cpp: promote class header in io to archetype

--- projects/dogen.text.cpp/src/types/transforms/io/class_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/io/class_header_transform.cpp
@@ -31,7 +31,6 @@
 #include "dogen.text.cpp/types/transforms/io/class_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::io {
-
 physical::entities::archetype class_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
