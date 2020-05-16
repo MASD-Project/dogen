e6e4f8b3fcf1e2a28229cef16a98737682b31b2c
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-16 18:22:31 +0100

    text.cpp: promote primitive implementation in io to archetype

--- projects/dogen.text.cpp/src/types/transforms/io/primitive_implementation_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/io/primitive_implementation_transform.cpp
@@ -37,7 +37,6 @@
 #include "dogen.text.cpp/types/transforms/io/primitive_implementation_transform.hpp"
 
 namespace dogen::text::cpp::transforms::io {
-
 physical::entities::archetype primitive_implementation_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
