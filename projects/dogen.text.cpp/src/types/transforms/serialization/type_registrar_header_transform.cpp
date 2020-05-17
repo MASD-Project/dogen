a56c90cdde80cc3ae187b424177b92d65ef95c7e
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-17 10:54:35 +0100

    text.cpp: promote type registrar header in serialisation to archetype

--- projects/dogen.text.cpp/src/types/transforms/serialization/type_registrar_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/serialization/type_registrar_header_transform.cpp
@@ -28,7 +28,6 @@
 #include "dogen.text.cpp/types/transforms/serialization/type_registrar_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::serialization {
-
 physical::entities::archetype type_registrar_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
