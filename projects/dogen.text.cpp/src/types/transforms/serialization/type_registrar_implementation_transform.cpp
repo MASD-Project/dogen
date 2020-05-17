5c71893def2f8a53e014f04972a2169b603c04ff
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-17 10:59:02 +0100

    text.cpp: promote type registrar implementation in serialisation to archetype

--- projects/dogen.text.cpp/src/types/transforms/serialization/type_registrar_implementation_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/serialization/type_registrar_implementation_transform.cpp
@@ -34,7 +34,6 @@
 #include "dogen.text.cpp/types/transforms/serialization/type_registrar_implementation_transform.hpp"
 
 namespace dogen::text::cpp::transforms::serialization {
-
 physical::entities::archetype type_registrar_implementation_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
