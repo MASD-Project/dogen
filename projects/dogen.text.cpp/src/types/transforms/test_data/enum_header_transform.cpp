27bf182d24bf68d792443b4416ad0a833569a4a4
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-17 10:09:07 +0100

    text.cpp: promote enum header in test data to archetype

--- projects/dogen.text.cpp/src/types/transforms/test_data/enum_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/test_data/enum_header_transform.cpp
@@ -31,7 +31,6 @@
 #include "dogen.text.cpp/types/transforms/test_data/enum_header_transform.hpp"
 
 namespace dogen::text::cpp::transforms::test_data {
-
 physical::entities::archetype enum_header_transform::static_archetype() const {
     static physical::entities::archetype r([]() {
         physical::entities::archetype r;
