9f1704c8d7965dd4c5bd9af9fc85876b4b6e24fb
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-25 17:41:59 +0100

    text.cpp: add tracing support to lexical cast facet

--- projects/dogen.text.cpp/src/types/transforms/lexical_cast/enum_header_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/lexical_cast/enum_header_transform.cpp
@@ -92,6 +92,9 @@ std::list<std::string> enum_header_transform::inclusion_dependencies(
 
 void enum_header_transform::apply(const context& ctx, const logical::entities::element& e,
     physical::entities::artefact& a) const {
+    tracing::scoped_transform_tracer stp(lg, "enum header transform",
+        transform_id, e.name().qualified().dot(), *ctx.tracer(), e);
+
     assistant ast(ctx, e, archetype().meta_name(), false/*requires_header_guard*/, a);
     const auto& enm(ast.as<logical::entities::structural::enumeration>(e));
 
@@ -148,6 +151,7 @@ ast.stream() << "}" << std::endl;
 ast.stream() << std::endl;
     } // sbf
     ast.update_artefact();
+    stp.end_transform(a);
 }
 
 }
