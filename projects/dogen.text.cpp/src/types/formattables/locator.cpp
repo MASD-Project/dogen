b470b1976f6320a7272bf56cc7b0a336889adb59
Marco Craveiro <marco.craveiro@gmail.com>
2020-05-30 13:41:25 +0100

    text.cpp: use the new is container property in locator

--- projects/dogen.text.cpp/src/types/formattables/locator.cpp
+++ projects/dogen.text.cpp/src/types/formattables/locator.cpp
@@ -343,11 +343,8 @@ boost::filesystem::path locator::make_facet_path(
      * Modules other than the model module contribute their simple
      * names to the directories.
      */
-    if (n != model_name_) {
-        const auto i(module_ids_.find(n.qualified().dot()));
-        if (i != module_ids_.end())
+    if (n != model_name_ && n.is_container())
             r /= n.simple();
-    }
 
     /*
      * Handle the file name.
@@ -396,11 +393,8 @@ boost::filesystem::path locator::make_facet_path_temp(
      * Modules other than the model module contribute their simple
      * names to the directories.
      */
-    if (n != model_name_) {
-        const auto i(module_ids_.find(n.qualified().dot()));
-        if (i != module_ids_.end())
-            r /= n.simple();
-    }
+    if (n != model_name_ && n.is_container())
+        r /= n.simple();
 
     /*
      * Handle the file name.
