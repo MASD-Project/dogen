2482fa3ed87c744b35e2a130d2fc3296f8159a04
Marco Craveiro <marco.craveiro@gmail.com>
2020-07-11 15:56:09 +0100

    orchestration: implement the artefact repository transform

--- projects/dogen.orchestration/src/types/transforms/physical_artefact_repository_transform.cpp
+++ projects/dogen.orchestration/src/types/transforms/physical_artefact_repository_transform.cpp
@@ -18,16 +18,21 @@
  * MA 02110-1301, USA.
  *
  */
+#include <boost/throw_exception.hpp>
 #include "dogen.utility/types/log/logger.hpp"
 #include "dogen.tracing/types/scoped_tracer.hpp"
 #include "dogen.text/io/entities/model_io.hpp"
 #include "dogen.physical/io/entities/artefact_repository_io.hpp"
+#include "dogen.logical/types/entities/element.hpp"
+#include "dogen.orchestration/types/transforms/transform_exception.hpp"
 #include "dogen.orchestration/types/transforms/physical_artefact_repository_transform.hpp"
 
 namespace {
 
 const std::string
 transform_id("orchestration.transforms.physical_artefact_repository_transform");
+
+const std::string duplicate_id("Duplicate logical name: ");
 
 using namespace dogen::utility::log;
 auto lg(logger_factory(transform_id));
@@ -36,13 +41,23 @@ auto lg(logger_factory(transform_id));
 
 namespace dogen::orchestration::transforms {
 
-physical::entities::artefact_repository
-physical_artefact_repository_transform::
+physical::entities::artefact_repository physical_artefact_repository_transform::
 apply(const text::transforms::context& ctx, const text::entities::model& m) {
-    tracing::scoped_transform_tracer stp(lg, "text model production chain",
+    tracing::scoped_transform_tracer stp(lg, "physical artefact repository",
         transform_id, m.name().qualified().dot(), *ctx.tracer(), m);
 
     physical::entities::artefact_repository r;
+    auto& asbli(r.artefact_sets_by_logical_id());
+    for (const auto& ea : m.elements()) {
+        const auto lid(ea.element()->name().qualified().dot());
+        const auto pair(std::make_pair(lid, ea.artefacts()));
+        const auto inserted(asbli.insert(pair).second);
+        if (inserted)
+            continue;
+
+        BOOST_LOG_SEV(lg, error) << duplicate_id << lid;
+        BOOST_THROW_EXCEPTION(transform_exception(duplicate_id + lid));
+    }
 
     stp.end_transform(r);
     return r;
