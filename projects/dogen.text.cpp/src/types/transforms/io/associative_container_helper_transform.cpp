0a5de9678dddfb450061b2f272a2d01e56581983
Marco Craveiro <marco.craveiro@gmail.com>
2020-11-13 16:44:39 +0000

    text.cpp: replace legacy associative container helper in io

--- projects/dogen.text.cpp/src/types/transforms/io/associative_container_helper_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/io/associative_container_helper_transform.cpp
@@ -18,27 +18,26 @@
  * MA 02110-1301, USA.
  *
  */
-#include "dogen.text.cpp/types/transforms/io/associative_container_helper.hpp"
+#include "dogen.text.cpp/types/transforms/io/associative_container_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/assistant.hpp"
 #include "dogen.text.cpp/types/transforms/io/traits.hpp"
 #include "dogen.text.cpp/types/transforms/types/traits.hpp"
 #include "dogen.logical/types/entities/helper_properties.hpp"
 
 namespace dogen::text::cpp::transforms::io {
-
-std::string associative_container_helper::id() const {
+std::string associative_container_helper_transform::id() const {
     static auto r(std::string("<") + traits::facet_qn() + std::string(">") +
         std::string("<") + helper_name() + std::string(">"));
     return r;
 }
 
-std::string associative_container_helper::family() const {
+std::string associative_container_helper_transform::family() const {
     static std::string r("AssociativeContainer");
     return r;
 }
 
 std::list<std::string>
-associative_container_helper::owning_formatters() const {
+associative_container_helper_transform::owning_formatters() const {
     static auto r(std::list<std::string> {
         io::traits::class_implementation_archetype_qn(),
         io::traits::primitive_implementation_archetype_qn(),
@@ -49,24 +48,24 @@ associative_container_helper::owning_formatters() const {
 }
 
 std::list<std::string>
-associative_container_helper::owning_facets() const {
+associative_container_helper_transform::owning_facets() const {
     static auto r(std::list<std::string> {
         io::traits::facet_qn(), types::traits::facet_qn()
     });
     return r;
 }
 
-std::string associative_container_helper::helper_name() const {
-    static std::string r("associative_container_helper");
+std::string associative_container_helper_transform::helper_name() const {
+    static std::string r("associative_container_helper_transform");
     return r;
 }
 
-bool associative_container_helper::is_enabled(const assistant& a,
+bool associative_container_helper_transform::is_enabled(const assistant& a,
     const logical::entities::helper_properties& hp) const {
     return a.is_streaming_enabled(hp);
 }
 
-void associative_container_helper::
+void associative_container_helper_transform::
 apply(assistant& ast, const logical::entities::helper_properties& hp) const {
     {
         const auto d(hp.current());
--- projects/dogen.text.cpp/src/types/transforms/io/initializer.cpp
+++ projects/dogen.text.cpp/src/types/transforms/io/initializer.cpp
@@ -27,7 +27,7 @@
 #include "dogen.text.cpp/types/transforms/io/enum_header_transform.hpp"
 #include "dogen.text.cpp/types/transforms/io/enum_implementation_transform.hpp"
 #include "dogen.text.cpp/types/transforms/io/smart_pointer_helper.hpp"
-#include "dogen.text.cpp/types/transforms/io/associative_container_helper.hpp"
+#include "dogen.text.cpp/types/transforms/io/associative_container_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/io/optional_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/io/pair_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/io/ptree_helper_transform.hpp"
@@ -49,7 +49,7 @@ void initializer::initialize(registrar& rg) {
     register_formatter<enum_implementation_transform>(rg);
 
     register_helper_formatter<smart_pointer_helper>(rg);
-    register_helper_formatter<associative_container_helper>(rg);
+    register_helper_formatter<associative_container_helper_transform>(rg);
     register_helper_formatter<optional_helper_transform>(rg);
     register_helper_formatter<pair_helper_transform>(rg);
     register_helper_formatter<ptree_helper_transform>(rg);
