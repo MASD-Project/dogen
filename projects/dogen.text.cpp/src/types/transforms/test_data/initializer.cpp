b390d9349482d2bf2604467f7b441a73207c38a3
Marco Craveiro <marco.craveiro@gmail.com>
2020-11-13 17:48:10 +0000

    text.cpp: replace legacy sequence container helper in test data

--- projects/dogen.text.cpp/src/types/transforms/test_data/initializer.cpp
+++ projects/dogen.text.cpp/src/types/transforms/test_data/initializer.cpp
@@ -37,7 +37,7 @@
 #include "dogen.text.cpp/types/transforms/test_data/path_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/ptime_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/ptree_helper_transform.hpp"
-#include "dogen.text.cpp/types/transforms/test_data/sequence_container_helper.hpp"
+#include "dogen.text.cpp/types/transforms/test_data/sequence_container_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/smart_pointer_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/string_helper.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/time_duration_helper.hpp"
@@ -66,7 +66,7 @@ void initializer::initialize(registrar& rg) {
     register_helper_formatter<path_helper_transform>(rg);
     register_helper_formatter<ptime_helper_transform>(rg);
     register_helper_formatter<ptree_helper_transform>(rg);
-    register_helper_formatter<sequence_container_helper>(rg);
+    register_helper_formatter<sequence_container_helper_transform>(rg);
     register_helper_formatter<smart_pointer_helper_transform>(rg);
     register_helper_formatter<string_helper>(rg);
     register_helper_formatter<time_duration_helper>(rg);
--- projects/dogen.text.cpp/src/types/transforms/test_data/sequence_container_helper_transform.cpp
+++ projects/dogen.text.cpp/src/types/transforms/test_data/sequence_container_helper_transform.cpp
@@ -18,26 +18,25 @@
  * MA 02110-1301, USA.
  *
  */
-#include "dogen.text.cpp/types/transforms/test_data/sequence_container_helper.hpp"
+#include "dogen.text.cpp/types/transforms/test_data/sequence_container_helper_transform.hpp"
 #include "dogen.text.cpp/types/transforms/assistant.hpp"
 #include "dogen.text.cpp/types/transforms/test_data/traits.hpp"
 #include "dogen.logical/types/entities/helper_properties.hpp"
 
 namespace dogen::text::cpp::transforms::test_data {
-
-std::string sequence_container_helper::id() const {
+std::string sequence_container_helper_transform::id() const {
     static auto r(std::string("<") + traits::facet_qn() + std::string(">") +
         std::string("<") + helper_name() + std::string(">"));
     return r;
 }
 
-std::string sequence_container_helper::family() const {
+std::string sequence_container_helper_transform::family() const {
     static std::string r("SequenceContainer");
     return r;
 }
 
 std::list<std::string>
-sequence_container_helper::owning_formatters() const {
+sequence_container_helper_transform::owning_formatters() const {
     static auto r(std::list<std::string> {
         traits::class_implementation_archetype_qn(),
         traits::primitive_implementation_archetype_qn()
@@ -46,24 +45,24 @@ sequence_container_helper::owning_formatters() const {
 }
 
 std::list<std::string>
-sequence_container_helper::owning_facets() const {
+sequence_container_helper_transform::owning_facets() const {
     static auto r(std::list<std::string> {
         traits::facet_qn()
     });
     return r;
 }
 
-std::string sequence_container_helper::helper_name() const {
-    static std::string r("sequence_container_helper");
+std::string sequence_container_helper_transform::helper_name() const {
+    static std::string r("sequence_container_helper_transform");
     return r;
 }
 
-bool sequence_container_helper::is_enabled(const assistant& /*a*/,
+bool sequence_container_helper_transform::is_enabled(const assistant& /*a*/,
     const logical::entities::helper_properties& /*hp*/) const {
     return true;
 }
 
-void sequence_container_helper::apply(assistant& ast, const logical::entities::helper_properties& hp) const {
+void sequence_container_helper_transform::apply(assistant& ast, const logical::entities::helper_properties& hp) const {
     const auto d(hp.current());
     const auto qn(d.name_tree_qualified());
     const auto ident(d.name_tree_identifiable());
