a30fba7b59234eff6d3569b478b5906d313b7d3c
Marco Craveiro <marco.craveiro@gmail.com>
2021-01-01 13:05:52 +0000

    codec: implement builder

--- projects/dogen.codec/include/dogen.codec/types/helpers/builder.hpp
+++ projects/dogen.codec/include/dogen.codec/types/helpers/builder.hpp
@@ -25,24 +25,88 @@
 #pragma once
 #endif
 
-#include <algorithm>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include "dogen.codec/types/entities/element.hpp"
+#include "dogen.codec/types/entities/object.hpp"
+#include "dogen.codec/types/entities/model.hpp"
 
 namespace dogen::codec::helpers {
 
+/**
+ * @brief Builds a codec model from objects.
+ *
+ * Expects the objects to have been supplied in dependency order.
+ */
 class builder final {
-public:
-    builder() = default;
-    builder(const builder&) = default;
-    builder(builder&&) = default;
-    ~builder() = default;
-    builder& operator=(const builder&) = default;
+private:
+    /*
+     * @brief Responsible for tracking the qualified name of the
+     * element for elements corresponding to Dia's UML large package,
+     * as well as an iterator to the element itself in the model.
+     */
+    struct uml_large_package_properties {
+        std::string name;
+        std::list<codec::entities::element>::iterator element;
+    };
 
 public:
-    bool operator==(const builder& rhs) const;
-    bool operator!=(const builder& rhs) const {
-        return !this->operator==(rhs);
-    }
+    explicit builder(
+        const std::unordered_map<std::string, std::list<std::string>>&
+        parent_id_to_child_ids);
 
+private:
+    /**
+     * @brief Updates the parenting information for the given object
+     * identified by the dia ID.
+     */
+    void update_parentage(const std::string& id, const std::string& n);
+
+    /**
+     * @brief Returns the list of names of parents for a given dia
+     * object id. If none exist, the list is empty.
+     */
+    const std::list<std::string>&
+    parents_for_object(const std::string& id) const;
+
+    /**
+     * @brief Returns the qualified name of the containing object, or
+     * an empty string.
+     */
+    std::string contained_by(const std::string& id) const;
+
+private:
+    /**
+     * @brief Handles the processing of UML large packages.
+     */
+    void handle_uml_large_package(const entities::object& o,
+        const std::string& n);
+
+    /**
+     * @brief Handles the processing of UML notes.
+     */
+    void handle_uml_note(const entities::object& o);
+
+public:
+    /**
+     * @brief Adds a processed object to the builder.
+     */
+    void add(const entities::object& o);
+
+    /**
+     * @brief Generates the external model.
+     */
+    codec::entities::model build();
+
+private:
+    const std::unordered_map<std::string, std::list<std::string>>&
+    parent_id_to_child_ids_;
+    std::unordered_map<std::string, uml_large_package_properties>
+    id_to_uml_large_package_properties_;
+    std::unordered_map<std::string, std::list<std::string>>
+    child_id_to_parent_names_;
+    codec::entities::model model_;
 };
 
 }
